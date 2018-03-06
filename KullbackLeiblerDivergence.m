3e038737417871bfd30c56a57472ec82b799befb
Emilio Isaías-Camacho <emilio.isaias-camacho@fau.de>
2018-03-06 15:46:08 +0100

    Validated for histogram divergence
    If there are zeros in the distribution, the program adds the minimum non-zero element to it and normalizes it once again.

--- KullbackLeiblerDivergence.m
+++ KullbackLeiblerDivergence.m
@@ -1,18 +1,21 @@
 function kbd = KullbackLeiblerDivergence(P1,P2)
 % KULLBACKLEIBLERDIVERGENCE returns the similarity measure between two
 % probability distributions
-if abs(sum(P1) - 1.0) > 1e-3
+if abs(sum(P1) - 1.0) > 1e-3 || sum(~P1)
+    P1 = P1 + min(P1(~(~(P1))));
     P1 = P1/sum(P1);
 end
 if length(P1) == length(P2)
     N = length(P1);
-    if abs(sum(P2) - 1) > 1e-3
+    if abs(sum(P2) - 1) > 1e-3 || sum(~P2)
+        P2 = P2 + min(P2(~(~(P2))));
         P2 = P2/sum(P2);
     end
 else
     kbd = NaN;
-    fprintf([],'! he distributions have different resolutions (different length)!\n')
+    fprintf('! The distributions have different resolutions (different length)!\n')
     return;
 end
-kbd = dot(P1,log2(P1 ./ P2))/log2(N);
+aux = rdivide(P1,P2);
+kbd = dot(P1,log2(aux))/log2(N);
 end
