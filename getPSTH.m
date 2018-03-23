87fed3ba620d76417e7f892e3928119fe8340b27
Emilio Isaías-Camacho <emilio.isaias-camacho@fau.de>
2018-03-23 08:19:27 +0100

    Improvements for generalization
    Returning a stack with all the trials and the eventualities in each alignment section.

--- getPSTH.m
+++ getPSTH.m
@@ -1,60 +1,93 @@
-function [PSTH,Trials] = getPSTH(spT,stimPeriods,whp,puffPeriods,LFP,...
-    winPre,winPost,fs)
-% GETSTA gets an average of the stimulus $s$ starting at time $t$ and
-% ending at time $t_0$, with a sampling period of $\delta$T. The time point
-% $t_0$ corresponds to the considered spike time $sp_i$ and the absolute
-% difference between $t$ and $t_0$ is 'win' in \milli seconds. Usually 10
-% ms.
-winel = round(winPre*fs); % window elements
-postSamples = round(winPost*fs);
-len = length(LFP);
-[PSTH,Trials] = getPeriStimuliSpikes(...
-    winel,postSamples,stimPeriods,whp,puffPeriods,spT,len,fs);
+function PSTHstack = getPSTH(spT,alignP,timeSpan,fs,consEvents)
+% GETPSTH returns a stack of spikes aligned to a certain event ''alignT''
+% considering the events ''consEvents''. 
 
+% Computing the size of the PSTH stack
+[auxR,auxC] = size(alignP);
+if auxR < auxC
+    alignP = alignP';
+end
+[Na, raf] = size(alignP);
+if raf > 2 || raf < 1
+    fprintf(['Warning! The alignment matrix is expected to have ',...
+        'either only rise or rise and falling edges time indices.\n'])
+    PSTHstack = NaN;
+    return;
+end
+if nargin == 5
+    Ne = length(consEvents);
+else
+    Ne = 0;
+end
+% Preallocation of the spike-stack:
+toi = sum(timeSpan);
+prevSamples = ceil(timeSpan(1) * fs);
+postSamples = ceil(timeSpan(2) * fs);
+Nt = ceil(toi*fs) + 1;
+PSTHstack = boolean(2+Ne,Nt,Na);
+for cap = 1:Na
+    segmIdxs = [alignP(cap,1)-prevSamples,alignP(cap,1)+postSamples];
+   
+    
 end
 
-function [SpSum, Trials] = getPeriStimuliSpikes(prevSamples,...
-    postSamples,stimTime,wh,puffPeriods,logSpTrain,sigLen,fs)
-% Initializing the output variables.
-puffOmit = 0.2; % Puff omitting window 100 ms
-SpSumW = zeros(1,prevSamples+postSamples+1);
-SpSumNW = zeros(1,prevSamples+postSamples+1);
-SpSumA = zeros(1,prevSamples+postSamples+1);
-Nst = sum(stimTime(1,:)>0);
-WT = 0;
-NWT = 0;
-AT = 0;
-puffLight = 0;
-for cst = 1:Nst
-    % Taking a look only at light stimuli lasting 0.5 seconds
-    if stimTime(2,cst) > 0.4 %&& stimTime(2,cst) < 0.6
-        segmIdxs = [stimTime(1,cst)-prevSamples,stimTime(1,cst)+postSamples];
-        if segmIdxs(1) < 1 || segmIdxs(2) > sigLen
-            fprintf('Window is outside the signal domain. Ignoring...\n')
-            continue;
-        else
-            if ~sum(puffPeriods(stimTime(1,cst)-round(puffOmit*fs):...
-                    stimTime(1,cst)+round(puffOmit*fs)))
-                spTrainSegment = logSpTrain(segmIdxs(1):segmIdxs(2));
-                SpSumA = SpSumA + spTrainSegment;
-                AT = AT + 1;
-                if wh(stimTime(1,cst))
-                    WT = WT + 1;
-                    SpSumW = SpSumW + spTrainSegment;
-                else
-                    NWT = NWT + 1;
-                    SpSumNW = SpSumNW + spTrainSegment;
-                end
-            else
-                puffLight = puffLight + 1;
-            end
-        end
-    end
-end
-fprintf('Puff: %d\n',puffLight)
-SpSum = [SpSumA;SpSumW;SpSumNW];
-Trials = [AT;WT;NWT];
-end
+% function [PSTH,Trials] = getPSTH(spT,stimPeriods,whp,puffPeriods,LFP,...
+%     winPre,winPost,fs)
+% % GETSTA gets an average of the stimulus $s$ starting at time $t$ and
+% % ending at time $t_0$, with a sampling period of $\delta$T. The time point
+% % $t_0$ corresponds to the considered spike time $sp_i$ and the absolute
+% % difference between $t$ and $t_0$ is 'win' in \milli seconds. Usually 10
+% % ms.
+% winel = round(winPre*fs); % window elements
+% postSamples = round(winPost*fs);
+% len = length(LFP);
+% [PSTH,Trials] = getPeriStimuliSpikes(...
+%     winel,postSamples,stimPeriods,whp,puffPeriods,spT,len,fs);
+% 
+% end
+% 
+% function [SpSum, Trials] = getPeriStimuliSpikes(prevSamples,...
+%     postSamples,stimTime,wh,puffPeriods,logSpTrain,sigLen,fs)
+% % Initializing the output variables.
+% puffOmit = 0.2; % Puff omitting window 100 ms
+% SpSumW = zeros(1,prevSamples+postSamples+1);
+% SpSumNW = zeros(1,prevSamples+postSamples+1);
+% SpSumA = zeros(1,prevSamples+postSamples+1);
+% Nst = sum(stimTime(1,:)>0);
+% WT = 0;
+% NWT = 0;
+% AT = 0;
+% puffLight = 0;
+% for cst = 1:Nst
+%     % Taking a look only at light stimuli lasting 0.5 seconds
+%     if stimTime(2,cst) > 0.4 %&& stimTime(2,cst) < 0.6
+%         segmIdxs = [stimTime(1,cst)-prevSamples,stimTime(1,cst)+postSamples];
+%         if segmIdxs(1) < 1 || segmIdxs(2) > sigLen
+%             fprintf('Window is outside the signal domain. Ignoring...\n')
+%             continue;
+%         else
+%             if ~sum(puffPeriods(stimTime(1,cst)-round(puffOmit*fs):...
+%                     stimTime(1,cst)+round(puffOmit*fs)))
+%                 spTrainSegment = logSpTrain(segmIdxs(1):segmIdxs(2));
+%                 SpSumA = SpSumA + spTrainSegment;
+%                 AT = AT + 1;
+%                 if wh(stimTime(1,cst))
+%                     WT = WT + 1;
+%                     SpSumW = SpSumW + spTrainSegment;
+%                 else
+%                     NWT = NWT + 1;
+%                     SpSumNW = SpSumNW + spTrainSegment;
+%                 end
+%             else
+%                 puffLight = puffLight + 1;
+%             end
+%         end
+%     end
+% end
+% fprintf('Puff: %d\n',puffLight)
+% SpSum = [SpSumA;SpSumW;SpSumNW];
+% Trials = [AT;WT;NWT];
+% end
 
 % function [pB,pT,pS] = estimateStimuliPDF(burstStim,tonicStim,stim)
 % M = 3;
