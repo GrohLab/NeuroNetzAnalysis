e26db8befbd11d2f0bd10ecd3690ed6e6d2370ea
Emilio Isaías-Camacho <emilio.isaias-camacho@fau.de>
2018-04-13 17:11:03 +0200

    Basic constructor

--- GeneralWaveform.m
+++ GeneralWaveform.m
@@ -2,16 +2,20 @@ classdef GeneralWaveform
     %GENERALWAVEFORM implements a class for the
     %   Detailed explanation goes here
     
-    properties 
-        Data
+    properties (SetAccess = 'private')
+        Data 
         NSamples
         SamplingFreq
         Time
     end
+    properties
+        Units
+    end
     
     methods
-        function obj = GeneralWaveform(data,samplingFreq)
-            %UNTITLED11 Construct an instance of this class
+        function obj = GeneralWaveform(data, samplingFreq, units)
+            %GENERALWAVEFORM Construct an instance of this class. It takes
+            %two arguments: data and samplingfrequency which
             %   Detailed explanation goes here
                 [rows, samples] = size(data);
                 if rows > samples
@@ -19,7 +23,16 @@ classdef GeneralWaveform
                 end                
                 obj.Data = data;
                 obj.SamplingFreq = samplingFreq;
-                
+                obj.NSamples = length(data);
+                obj.Time =...
+                    0:1/obj.SamplingFreq:obj.(NSamples-1)/obj.SamplingFreq;
+                if nargin == 3
+                    if ischar(units)
+                        obj.Units = units;
+                    else
+                        warning('String expected')
+                    end
+                end
         end
         
         function outputArg = method1(obj,inputArg)
@@ -27,6 +40,8 @@ classdef GeneralWaveform
             %   Detailed explanation goes here
             outputArg = obj.Property1 + inputArg;
         end
+        
+        
     end
 end
 
