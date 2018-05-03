7cc83b9afaf026f14dc1a1f1009b09a28f20eb88
Emilio Isaías-Camacho <emilio.isaias-camacho@fau.de>
2018-05-03 17:28:35 +0200

    Working bilinear filter
    Computationally expensive. The implementation needs optimizations

--- bilinearFilter.m
+++ bilinearFilter.m
@@ -1,21 +1,35 @@
-function [f_new, flt] = bilinearFilter(wind_size, signal)
+function [f_new] = bilinearFilter(wind_size, signal)
 % BILINEARFILTER performs a filtering operation which preserves the edges
 % information and reduces the noise in flat areas.
 %   [smth, flt] = bilinearFilter(window_size, signal)
 
-cw = (wind_size/2) +1;
+cw = floor(wind_size/2) +1;
 f_new = zeros(size(signal));
-s = @(f1,f2) (exp(-(abs(repmat(f1,size(f2))-f2)/std(f2))/2).^2);
-while cw + 2^4 <= N
-    sum = 0;
-    for mu = (-2^4):(2^4)
-        sum = sum + signal(cw+mu)*...
-            s(signal(cw),signal(cw+mu),var(signal((cw-2^4):(cw+2^4))))*...
-            s(cw,cw+mu,var((cw-2^4):(cw+2^4)));
-    end
-    f_new(cw) = sum;
-    cw = cw + 1;
+s = @(f1,f2) (exp((-(abs(repmat(f1,size(f2))-f2)/std(f2))/2).^2));
+N = length(signal);
+winHalf = floor(wind_size/2);
+h = waitbar(0,'Bilinear filter...');
+while cw+3 + winHalf <= N
+    similarity1 = s(signal(cw),signal(cw-winHalf:cw+winHalf));
+    similarity2 = s(signal(cw+1),signal(cw+1-winHalf:cw+1+winHalf));
+    similarity3 = s(signal(cw+2),signal(cw+2-winHalf:cw+2+winHalf));
+    similarity4 = s(signal(cw+3),signal(cw+3-winHalf:cw+3+winHalf));
+    closeness1 = s(cw,cw-winHalf:cw+winHalf);
+    closeness2 = s(cw+1,cw+1-winHalf:cw+1+winHalf);
+    closeness3 = s(cw+2,cw+2-winHalf:cw+2+winHalf);
+    closeness4 = s(cw+3,cw+3-winHalf:cw+3+winHalf);
+    f_new(cw) = sum(similarity1.*signal(cw-winHalf:cw+winHalf).*...
+        closeness1)/(similarity1*closeness1');
+    f_new(cw+1) = sum(similarity2.*signal(cw+1-winHalf:cw+1+winHalf).*...
+        closeness2)/(similarity2*closeness2');
+    f_new(cw+2) = sum(similarity3.*signal(cw+2-winHalf:cw+2+winHalf).*...
+        closeness3)/(similarity3*closeness3');
+    f_new(cw+3) = sum(similarity4.*signal(cw+3-winHalf:cw+3+winHalf).*...
+        closeness4)/(similarity4*closeness4');
+    cw = cw + 4;
+    waitbar(cw/(N-winHalf))
 end
+close(h)
 % s = zeros(1,wind_size);
 % c = s;
 % flt = zeros(size(signal));
