3a13a758ee8ea3fa47af124d86f3906b2cc48227
Emilio Isaías-Camacho <emilio.isaias-camacho@fau.de>
2018-05-10 14:07:11 +0200

    Re-organization of the data

--- genP_x.m
+++ genP_x.m
@@ -1,6 +1,9 @@
 function [ p_x, origSum ] = genP_x( params, x_axis)
 %genP_x Generates the estimated pdf for the data from emforgmm
 [M,~] = size(params);
+if iscolumn(x_axis)
+    x_axis = x_axis';
+end
 p_x = zeros(1,length(x_axis));
 for m = 1:M
     p_x = p_x + params(m,1)*evalgauss(x_axis,params(m,2),params(m,3));
