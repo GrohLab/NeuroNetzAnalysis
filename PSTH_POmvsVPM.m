ba41bbf07b83ee7d2cde08d7072438477a2d8e56
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2018-05-31 15:14:19 +0200

    Logical Bug fixing

--- PSTH_POmvsVPM.m
+++ PSTH_POmvsVPM.m
@@ -1,47 +1,80 @@
+%% Initialization
 ToniDir = '.\Database\EphysData\AnalysisMatFiles';
-fullfile(ToniDir,'*analysis.mat')
+expFiles = dir(fullfile(ToniDir,'*analysis.mat'));
 UMS = false;
 logFile = 'TONI-LOG.txt';
+m = 1e-3;
+fs = 2e4;
+
+EphysPath = '.\Database\EphysData\';
+DBPath = getParentDir(EphysPath,1);
+try
+    load([DBPath, 'ephys_database.mat'])
+catch ME
+    disp(ME.getReport)
+    disp('The Database couldn''t be loaded!')
+end
+RecDB.Properties.RowNames = strcat(RecDB.AnimalName,{'_'},...
+    datestr(RecDB.StartTime, 'HH_MM_SS'));
+try
+    load([EphysPath,'DiscreteData.mat'], 'DiscreteData');
+catch ME
+    disp(ME.getReport)
+    disp('The discrete data couldn''t be loaded')
+end
+%% Running through the *analysis.mat files in Toni's Directory
 for cf = 1:numel(expFiles)
     [~,baseName,~]=fileparts(expFiles(cf).name);
     baseName = baseName(1:end-8);
     if mkdir(ToniDir,baseName)
-        if movefile(fullfile(ToniDir,expFiles(cf).name),...
-                fullfile(ToniDir,basename))
-            fileName = fullfile(ToniDir,basename,expFiles(cf).name);
-            try
-                load(fileName,'filteredResponse','spikeFindingData',...
-                    'Triggers','EEG')
-                if ~isempty(filteredResponse.data)
-                    spkWvf = SpikeWaveform(filteredResponse.data,2e4);
-                    spkWvf.getSpikes_UMS;
-                    spT = spkWvf.Triggers;
-                    UMSSpikeStruct = spkWvf.UMSdata.SpikeUMSStruct;
-                    UMS = true;
-                elseif ~isempty(spikeFindingData.spikes)
-                    writeLogFile(logFile,[baseName,...
-                        ' no filtered response found.'])
-                    spT = find(strcmp(RecDB.Properties.RowNames, baseName));
-                    UMS = false;
-                end
-            catch
-                writeLogFile(logFile,[expFiles(cf).name,...
-                    ' unreadable!'])
-                continue
+        if ~exist(fullfile(ToniDir,baseName,expFiles(cf).name),'file')
+            if ~movefile(fullfile(ToniDir,expFiles(cf).name),...
+                    fullfile(ToniDir,basename))
+                writeLogFile(logFile,[expFiles(cf).name, ' couldn''t be found'])
+                continue;
             end
-            try
-                if UMS
-                    save(fileName,'UMSSpikeStruct','a')
-                end
-            catch
-                writeLogFile(logFile,[fileName,...
-                    ' unwritable!'])
-            end
-            cellType = RecDB(baseName,'PhysioNucleus');
-            [PSTHstack,LFPstack,~] = getPSTH(spT,Tirggers.whisker,...
-                [100,200]*1e-3,2e4,EEG.data,{});
-        else
         end
+        fileName = fullfile(ToniDir,basename,expFiles(cf).name);
+        try
+            load(fileName,'filteredResponse','Triggers','EEG')
+            if ~isempty(filteredResponse.data)
+                spkWvf = SpikeWaveform(filteredResponse.data,fs);
+                spkWvf.getSpikes_UMS;
+                spT = spkWvf.Triggers;
+                UMSSpikeStruct = spkWvf.UMSdata.SpikeUMSStruct;
+                UMS = true;
+            elseif ~isempty(UMS)
+                writeLogFile(logFile,[baseName,...
+                    ' no filtered response found.'])
+                spT = find(strcmp(RecDB.Properties.RowNames, baseName));
+                
+                UMS = false;
+            end
+        catch
+            writeLogFile(logFile,[expFiles(cf).name,...
+                ' unreadable!'])
+            continue
+        end
+        try
+            if UMS
+                save(fileName,'UMSSpikeStruct','a')
+            end
+        catch
+            writeLogFile(logFile,[fileName,...
+                ' unwritable!'])
+        end
+        cellType = RecDB(baseName,'PhysioNucleus');
+        % Whisker onset
+        RaF = whiskWf.Triggers;
+        whiskerMovement = Triggered
+        [PSTHstack1,LFPstack1,~] = getPSTH(spT,Raf(:,1),...
+            [100,200]*m,fs,EEG.data,{});
+        % Whisker offset
+        whiskWf = StepWaveform(Triggers.whisker,fs);
+        [PSTHstack2,LFPstack2,~] = getPSTH(spT,RaF(:,2),[100,200]*m,...
+            fs,EEG.data,{});
+        
+        
     else
     end
 end
