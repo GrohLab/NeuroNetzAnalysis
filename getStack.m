f8af4a860ae54c3d52c226805689f764ca190e2b
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2018-06-08 18:29:24 +0200

    Including the offset triggering

--- getStack.m
+++ getStack.m
@@ -1,5 +1,5 @@
 function [expStack, LFPstack, Wstack] =...
-    getStack(spT,alignP,timeSpan,fs,LFP,whiskerMovement,consEvents)
+    getStack(spT,alignP,ONOFF,timeSpan,fs,LFP,whiskerMovement,consEvents)
 % GETPSTH returns a stack of spikes aligned to a certain event ''alignT''
 % considering the events in the cell array ''consEvents''.
 
@@ -8,7 +8,15 @@ if isa(alignP,'logical')
     alWf = StepWaveform(alignP,fs,'on-off','Align triggers');
     alignP = alWf.Triggers;
 end
-
+switch ONOFF
+    case 'on'
+        disp('Considering onset of the triggers')
+    case 'off'
+        disp('Considering offset of the triggers')
+    otherwise
+        disp('Unrecognized trigger selection. Considering onsets')
+        ONOFF = 'on';
+end
 [auxR,auxC] = size(alignP);
 if auxR < auxC
     alignP = alignP';
@@ -23,7 +31,7 @@ end
 
 %% Considered events arrangement.
 Ne = 0;
-if nargin == 7
+if nargin == 8
     typ = whos('consEvents');
     switch typ.class
         case 'double'
@@ -71,23 +79,29 @@ if isnumeric(spT)
 end
 %% Cutting the events into the desired segments.
 for cap = 1:Na
-    segmIdxs = [alignP(cap,1)-prevSamples,alignP(cap,1)+postSamples];
+    if strcmp(ONOFF, 'on')
+        segmIdxs = [alignP(cap,1)-prevSamples,alignP(cap,1)+postSamples];
+    elseif strcmp(ONOFF, 'off')
+        segmIdxs = [alignP(cap,2)-prevSamples,alignP(cap,2)+postSamples];
+    end
     % The segments should be in the range of the spike train.
     if segmIdxs(1) >= 1 && segmIdxs(2) <= length(spT)
         spSeg = spT(segmIdxs(1):segmIdxs(2));
-        segmIdxsLFP = [(alignP(cap,1)*fsConv)-prevSamplesLFP,...
-            (alignP(cap,1)*fsConv)+postSamplesLFP];
+        segmIdxsLFP = round([(alignP(cap,1)*fsConv)-prevSamplesLFP,...
+            (alignP(cap,1)*fsConv)+postSamplesLFP]);
     else
         Na = Na - 1;
         continue;
     end
     expStack(2,:,cap) = spSeg;
     % Find 'overlapping' periods in time of interest
-    alignPeriod = getEventPeriod(alignP,{alignP},cap,prevSamples,postSamples);
+    alignPeriod = getEventPeriod(alignP, {alignP}, ONOFF, cap,...
+        prevSamples, postSamples);
     expStack(1,:,cap) = alignPeriod;
     if Ne
         expStack(3:2+Ne,:,cap) =...
-            getEventPeriod(alignP, consEvents, cap, prevSamples, postSamples);
+            getEventPeriod(alignP, consEvents, ONOFF, cap,...
+            prevSamples, postSamples);
     end
     % Getting the LFP segments taking into account the different sampling
     % frequencies i.e. LFP-->1 kHz Spikes --> 20 kHz HARD CODE!! BEWARE!!
@@ -102,7 +116,7 @@ end
 
 % Aligning the events according to the considered time point. The inputs
 % are time indices called Tdx as in Time inDeX.
-function evntOn = getEventPeriod(alignTdx, evntTdx, cap, prev, post)
+function evntOn = getEventPeriod(alignTdx, evntTdx, ONOFF, cap, prev, post)
 % Assuming that the considered events are always cells.
 if isempty(evntTdx)
     evntOn = [];
@@ -110,11 +124,15 @@ if isempty(evntTdx)
 else
     evntOn = false(numel(evntTdx),prev+post+1);
     for ce = 1:length(evntTdx)
-        relTdx = evntTdx{ce}(:,1) - alignTdx(cap,1);
+        if strcmp(ONOFF,'on')
+            relTdx = evntTdx{ce}(:,1) - alignTdx(cap,1);
+        else
+            relTdx = evntTdx{ce}(:,2) - alignTdx(cap,2);
+        end
         inToi = find(relTdx >= -prev & relTdx < post);
         if ~isempty(inToi)
             psthIdx = evntTdx{ce}(inToi,1)-alignTdx(cap,1) + prev + 1;
-            lenIdx = evntTdx{ce}(inToi,2);
+            lenIdx = evntTdx{ce}(inToi,2)-evntTdx{ce}(inToi,1) + 1;
             for cep = 1:numel(psthIdx)
                 idxs = psthIdx(cep):psthIdx(cep)+lenIdx(cep)-1;
                 idxs = idxs(idxs <= post+prev+1);
@@ -125,75 +143,3 @@ else
 end
 end
 
-
-% function [PSTH,Trials] = getPSTH(spT,stimPeriods,whp,puffPeriods,LFP,...
-%     winPre,winPost,fs)
-% % GETSTA gets an average of the stimulus $s$ starting at time $t$ and
-% % ending at time $t_0$, with a sampling period of $\delta$T. The time point
-% % $t_0$ corresponds to the considered spike time $sp_i$ and the absolute
-% % difference between $t$ and $t_0$ is 'win' in \milli seconds. Usually 10
-% % ms.
-% winel = round(winPre*fs); % window elements
-% postSamples = round(winPost*fs);
-% len = length(LFP);
-% [PSTH,Trials] = getPeriStimuliSpikes(...
-%     winel,postSamples,stimPeriods,whp,puffPeriods,spT,len,fs);
-%
-% end
-%
-% function [SpSum, Trials] = getPeriStimuliSpikes(prevSamples,...
-%     postSamples,stimTime,wh,puffPeriods,logSpTrain,sigLen,fs)
-% % Initializing the output variables.
-% puffOmit = 0.2; % Puff omitting window 100 ms
-% SpSumW = zeros(1,prevSamples+postSamples+1);
-% SpSumNW = zeros(1,prevSamples+postSamples+1);
-% SpSumA = zeros(1,prevSamples+postSamples+1);
-% Nst = sum(stimTime(1,:)>0);
-% WT = 0;
-% NWT = 0;
-% AT = 0;
-% puffLight = 0;
-% for cst = 1:Nst
-%     % Taking a look only at light stimuli lasting 0.5 seconds
-%     if stimTime(2,cst) > 0.4 %&& stimTime(2,cst) < 0.6
-%         segmIdxs = [stimTime(1,cst)-prevSamples,stimTime(1,cst)+postSamples];
-%         if segmIdxs(1) < 1 || segmIdxs(2) > sigLen
-%             fprintf('Window is outside the signal domain. Ignoring...\n')
-%             continue;
-%         else
-%             if ~sum(puffPeriods(stimTime(1,cst)-round(puffOmit*fs):...
-%                     stimTime(1,cst)+round(puffOmit*fs)))
-%                 spTrainSegment = logSpTrain(segmIdxs(1):segmIdxs(2));
-%                 SpSumA = SpSumA + spTrainSegment;
-%                 AT = AT + 1;
-%                 if wh(stimTime(1,cst))
-%                     WT = WT + 1;
-%                     SpSumW = SpSumW + spTrainSegment;
-%                 else
-%                     NWT = NWT + 1;
-%                     SpSumNW = SpSumNW + spTrainSegment;
-%                 end
-%             else
-%                 puffLight = puffLight + 1;
-%             end
-%         end
-%     end
-% end
-% fprintf('Puff: %d\n',puffLight)
-% SpSum = [SpSumA;SpSumW;SpSumNW];
-% Trials = [AT;WT;NWT];
-% end
-
-% function [pB,pT,pS] = estimateStimuliPDF(burstStim,tonicStim,stim)
-% M = 3;
-% err = 1e-3;
-% xLow = min(stim);
-% xHigh = max(stim);
-% dataRange = [xLow,xHigh];
-% prB = emforgmm(burstStim,M,err,0);        % Stimuli PDF for busrts
-% prT = emforgmm(tonicStim,M,err,0);  % Stimuli PDF for tonic sp
-% prS = emforgmm(stim,M,err,0);        % Stimuli PDF for non-sp
-% pB = gmmpdf(prB,dataRange);
-% pT = gmmpdf(prT,dataRange);
-% pS = gmmpdf(prS,dataRange);
-% end
