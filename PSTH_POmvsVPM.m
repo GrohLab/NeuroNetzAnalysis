f32a5505819b3e2aca00380cd58a4fdc7ead809b
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2018-06-14 16:58:15 +0200

    Included the folder immersion and good to run
    Finished implementing the script to run along all the experiments.

--- PSTH_POmvsVPM.m
+++ PSTH_POmvsVPM.m
@@ -1,7 +1,7 @@
 %% Initialization
 clearvars
 ToniDir = '.\Database\EphysData\AnalysisMatFiles';
-expFiles = dir(fullfile(ToniDir,'*analysis.mat'));
+expFiles = dir(fullfile(ToniDir,'*_00*'));
 UMS = false;
 logFile = 'TONI-LOG.txt';
 m = 1e-3;
@@ -25,41 +25,68 @@ catch ME
 end
 %% Running through the *analysis.mat files in Toni's Directory
 for cf = 1:numel(expFiles)
-    [~,baseName,~]=fileparts(expFiles(cf).name);
+    if expFiles(cf).isdir
+        analysisFile = dir(fullfile(ToniDir,expFiles(cf).name,'*analysis.mat'));
+        analysisFile = fullfile(analysisFile.folder,analysisFile.name);
+        [~,baseName,~] = fileparts(analysisFile);
+    else
+        [~,baseName,~]=fileparts(expFiles(cf).name);
+        analysisFile = fullfile(ToniDir,expFiles(cf).name);
+    end
     baseName = baseName(1:end-8);
     if mkdir(ToniDir,baseName)
-        if ~exist(fullfile(ToniDir,baseName,expFiles(cf).name),'file')
+        expDir = fullfile(ToniDir,baseName);
+        if ~exist(analysisFile,'file')
             disp(['Trying to move file ',expFiles(cf).name])
             if ~movefile(fullfile(ToniDir,expFiles(cf).name),...
-                    fullfile(ToniDir,basename))
+                    fullfile(ToniDir,baseName))
                 writeLogFile(logFile,[expFiles(cf).name, ' couldn''t be found'])
                 continue;
             end
         end
-        fileName = fullfile(ToniDir,basename,expFiles(cf).name);
+        fileName = analysisFile;
         try
-            load(fileName,'filteredResponse','Triggers','EEG')
-            if ~isempty(filteredResponse.data)
-                spkWvf = SpikeWaveform(filteredResponse.data,fs);
-                spkWvf.getSpikes_UMS;
-                spT = spkWvf.Triggers;
-                UMSSpikeStruct = spkWvf.UMSdata.SpikeUMSStruct;
-                UMS = true;
-            elseif ~isempty(UMS)
-                writeLogFile(logFile,[baseName,...
-                    ' no filtered response found.\t Using Toni''s extracted time stamps'])
-                expIdx = find(strcmp(RecDB.Properties.RowNames, baseName));
-                spT = DiscreteData(expIdx).Spikes;
-                UMS = false;
+            % Load the spike times from the UMS structure and use it
+            % directly to compute the stack.
+            if exist('UMSSpikeStruct','var')
+                clearvars UMSSpikeStruct
             end
+            load(fileName,'UMSSpikeStruct')
+            spT = round(UMSSpikeStruct.spiketimes * UMSSpikeStruct.params.Fs);
+            load(fileName,'Triggers','EEG')
+            UMS = false;
         catch
-            writeLogFile(logFile,[expFiles(cf).name,...
-                ' unreadable!'])
-            continue
+            try
+                load(fileName,'filteredResponse','Triggers','EEG')
+                if ~isempty(filteredResponse.data)
+                    spkWvf = SpikeWaveform(filteredResponse.data,fs);
+                    spkWvf.getSpikes_UMS;
+                    spT = spkWvf.Triggers;
+                    UMSSpikeStruct = spkWvf.UMSdata.SpikeUMSStruct;
+                    h = spkWvf.plot;
+                    try
+                        while strcmp(h.BeingDeleted,'off')
+                            waitforbuttonpress
+                        end
+                    catch
+                    end
+                    UMS = true;
+                elseif ~isempty(UMS)
+                    writeLogFile(logFile,[baseName,...
+                        ' no filtered response found.\t Using Toni''s extracted time stamps'])
+                    expIdx = find(strcmp(RecDB.Properties.RowNames, baseName));
+                    spT = DiscreteData(expIdx).Spikes;
+                    UMS = false;
+                end
+            catch
+                writeLogFile(logFile,[expFiles(cf).name,...
+                    ' unreadable!'])
+                continue
+            end
         end
         try
             if UMS
-                save(fileName,'UMSSpikeStruct','a')
+                save(fileName,'UMSSpikeStruct','-append')
                 writeLogFile(logFile,[fileName,...
                 ' UMS2k structure appended'])
             end
@@ -78,24 +105,67 @@ for cf = 1:numel(expFiles)
         allEvents = struct2cell(Triggers);
         consEvents = allEvents(~ismember(fieldNames,{'whisking','whisker'}));
         binningTime = 50*m;
+        
         % Whisker onset
-        [PSTHstackW,LFPstackW,WstackW] = getStack(spT,RaF,'on',...
-            timeLapse,fs,EEG.data,Triggers.whisking,consEvents);
-        [~,FigID] =...
+        [PSTHstackW,LFPstackW,WstackW] = getStack(spT, RaF, 'on',...
+            timeLapse, fs, EEG.data, Triggers.whisking, consEvents);
+        [~,FigIDW,kO] =...
             plotTriggeredEvents(PSTHstackW, LFPstackW, WstackW,...
             timeLapse, cellType, binningTime, fs);
-        
+        savePDF_FIG(FigIDW,'_W',expDir,baseName)
+%         if ~exist(fullfile(expDir,[baseName,'_W.pdf']),'file')
+%             print(FigIDW,fullfile(expDir,[baseName,'_W.pdf']),...
+%                 '-dpdf','-fillpage')
+%             savefig(FigIDW,fullfile(expDir,[baseName,'_W.fig']))
+%         end
+        close(FigIDW)
         % Whisker offset
         [PSTHstackNW,LFPstackNW,WstackNW] = getStack(spT, RaF, 'off',...
             timeLapse, fs, EEG.data, Triggers.whisking, consEvents);
-        [~,FigID] =...
+        [~,FigIDNW] =...
             plotTriggeredEvents(PSTHstackNW, LFPstackNW, WstackNW,...
             timeLapse, cellType, binningTime, fs);
-
+        savePDF_FIG(FigIDNW,'_NW',expDir,baseName)
+%         if ~exist(fullfile(expDir,[baseName,'_NW.pdf']),'file')
+%             print(FigIDNW,fullfile(expDir,[baseName,'_NW.pdf']),...
+%                 '-dpdf','-fillpage')
+%             savefig(FigIDNW,fullfile(expDir,[baseName,'_NW.fig']))
+%         end
+        close(FigIDNW)
+        % Sanity plot
+        figIDS = figure('Name',[char(cellType), ' considered events'],...
+            'Color',[1,1,1]);
+        try
+            subplot(2,1,1);mesh(squeeze(sum(PSTHstackW(3:end,:,:),2)));ylabel('Event')
+            box off;xlabel('Alignment point');zlabel('Count');
+            title('Segmented events in stack')
+            subplot(2,1,2);mesh(squeeze(sum(PSTHstackW(3:end,:,~kO),2)))
+            title('Events to include in the PSTH');ylabel('Event');box off
+            xlabel('Alignment point');zlabel('Count');
+        catch
+            disp('Maybe there is no ''clean'' data')
+            writeLogFile(logFile,[fullfile(ToniDir,baseName), ' sanity plot not created'])
+        end
+        savePDF_FIG(figIDS,'_consideredEvents',expDir,baseName)
+%         if exist(fullfile(expDir,[baseName,'consideredEvents.pdf']),'file')
+%             print(figIDS,fullfile(expDir,[baseName,'consideredEvents.pdf']),...
+%                 '-dpdf','-fillpage')
+%             savefig(figIDS,fullfile(expDir,[baseName,'consideredEvents.fig']))
+%         end
+        close(figIDS)
     else
         writeLogFile(logFile,[fullfile(ToniDir,baseName), ' couldn''t be created'])
         disp([fullfile(ToniDir,baseName), ' couldn''t be created'])
     end
+end
+
+function savePDF_FIG(h,postfix,path,bName)
+if ~exist(fullfile(path,[bName,postfix,'.pdf']),'file') ||...
+        ~exist(fullfile(path,[bName,postfix,'.fig']),'file')
+    print(h,fullfile(path,[bName,postfix,'.pdf']),...
+        '-dpdf','-fillpage')
+    savefig(h,fullfile(path,[bName,postfix,'.fig']))
+end
 end
 
 function myStatus = writeLogFile(logFileName,myMessage)
