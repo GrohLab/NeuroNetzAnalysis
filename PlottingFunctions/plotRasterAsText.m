45c954dc72f980297c66c4a73f593f3541bcc4be
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2018-07-06 10:56:36 +0200

    Plot raster as text and as a graphical object

--- PlottingFunctions/plotRasterAsText.m
+++ PlottingFunctions/plotRasterAsText.m
@@ -1,5 +1,5 @@
-function [outputArg1,outputArg2] =...
-    plotRasterFromStack(discreteStack, timeLapse, fs, figTitle, ax)
+function [ax] =...
+    plotRasterAsText(relativeSpikeTimes, timeLapse, fs, figTitle, ax)
 %UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -7,54 +7,45 @@ function [outputArg1,outputArg2] =...
 % stack
 % Nt is the time axis for the chosen window
 % Na is the number of alignment points in this stack
-[Ne, Nt, Na] = size(discreteStack);
+[Ne, Na] = size(relativeSpikeTimes);
 % Time axis
+idxOffset = timeLapse * fs;
+Nt = sum(idxOffset) + 1;
 tx = 0:1/fs:(Nt-1)/fs;
 tx = tx - timeLapse(1);
-AX_FLAG = true;
-if ~exist('ax','var') || isempty(ax)
-    figure('Name',['Raster Plot ', figTitle],'Color',[1,1,1]);
-    AX_FLAG = false;
+if ~exist('figTitle','var')
+    figTitle = [];
 end
-cmap = colormap(jet(Ne-1));
-FIRST_FLAG = true;
-for cse = 2:Ne
+plotTitle = ['Raster Plot ', figTitle];
+if ~exist('ax','var') || isempty(ax)
+    figure('Name',plotTitle,'Color',[1,1,1]);plot(NaN,NaN);
+    ax = gca;
+end
+cmap = colormap(jet(Ne));
+yTickLabel = cell(1,Ne);
+for cse = 1:Ne
     % For each spike train
+    yTickLabel(cse) = {['Neuron ',num2str(cse)]};
     for cap = Na:-1:1
         % For each alignment point
-        upEdge = diff(discreteStack(cse,:,cap)) > 0;
-        if sum(upEdge) ~= 0
-            % If there are events in this alignment point
-            downEdge = diff(discreteStack(cse,:,cap)) < 0;
-            isSpike = upEdge(1:end-1) - downEdge(2:end);
-            if sum(isSpike) == 0
-                % If the event contains spikes
-                if FIRST_FLAG
-                    hold on
-                    FIRST_FLAG = false;
-                end
-                xspks = tx(squeeze(discreteStack(cse,:,cap)));
-                lvl = (cse - 2)*Na + cap;
-                if AX_FLAG
-                    plot(ax,xspks,lvl*ones(1,numel(xspks)),...
-                        'LineStyle','none','Marker','.',...
-                        'MarkerFaceColor',cmap(cse-1,:),'MarkerSize',2)
-                else
-                    plot(xspks,lvl*ones(1,numel(xspks)),...
-                        'LineStyle','none','Marker','.',...
-                        'MarkerFaceColor',cmap(cse-1,:),'MarkerSize',2,...
-                        'Color',cmap(cse-1,:))
-                end
-                for cs = 1:numel(xspks)
-                    % For each spike
-                    text(ax, xspks(cs),cap*(cse-1),'.',...
-                        'HorizontalAlignment','center','Color', cmap(cap))
-                end
+        if ~isempty(relativeSpikeTimes{cse,cap})
+            xspks = tx(relativeSpikeTimes{cse,cap} + idxOffset(1) + 1);
+            lvl = (cse - 1)*Na + cap;
+            for cs = 1:numel(xspks)
+                % For each spike
+                text(ax, xspks(cs),lvl,'.',...
+                    'HorizontalAlignment','center','Color', cmap(cse,:))
             end
         end
     end
 end
-axis([-timeLapse(1),timeLapse(2),1,Na*(Ne-1)])
+
+axis([-timeLapse(1),timeLapse(2),1,Na*Ne])
+ax = gca;set(ax,'Box','off','YTick',(0:Ne)*Na + Na/2,'YTickLabel',yTickLabel,...
+    'YTickLabelRotation',90)
+xlabel(ax,'Time [s]')
+title(ax,figTitle,'Interpreter','none')
+
 
 end
 
