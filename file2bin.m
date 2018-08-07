59851e7660f8741d8facbab0afb5279d967a6d4e
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2018-08-07 17:08:46 +0200

    Finilizing the function and testing

--- file2bin.m
+++ file2bin.m
@@ -10,45 +10,45 @@ function file2bin(fileInput, fileOutput)
 % unability to save the sampling frequency. One solution to this problem
 % might be that the sampling frequency is at the very beguinning of the
 % file, in the first channel, in the first position.
-if nargin == 2
-    % The file output was given.
-    fprintf('Reading %s file...\n',fileInput)
-    if exist(fileInput,'file')
-        [flDir, baseName, flEx] = fileparts(fileInput);
-        switch flEx
-            % Depending on the file extension, the function will import and
-            % save the binary file in different manners
-            case {'.smr','.smrx'}
-                fprintf('spike2 file format recognized')
-                crrDir = pwd;
-                importSMR([baseName,flEx],flDir,1);
-                cd(crrDir)
-                mat2bin(fullfile(flDir,[baseName,'.mat']),fileOutput)
-            case '.mat'
-                fprintf('.mat file recognized')
-                if contains(baseName,'analysis')
-                    fprintf('The analysis file is not meant to be imported\n')
-                    baseName = strsplit(baseName,'analysis');
-                    baseName = baseName{1};
-                end
-                fprintf('Importing %s...\n',[baseName,flEx])
-                
-            otherwise
-                fprintf('This input file is not recognized.')
-        end
-    else
-        fprintf('The input file was not found')
-    end
-elseif nargin < 2
     
+if isstring(fileInput)
+    fileInput = char(fileInput);
 end
-
+fprintf('Reading %s file...\n',fileInput)
+if exist(fileInput,'file')
+    [flDir, baseName, flEx] = fileparts(fileInput);
+    if nargin < 2
+        fileOutput = fullfile(flDir,[baseName,'.bin']);
+    end
+    switch flEx
+        % Depending on the file extension, the function will import and
+        % save the binary file in different manners
+        case {'.smr','.smrx'}
+            fprintf('spike2 file format recognized\n')
+            crrDir = cd;
+            importSMR([baseName,flEx],flDir,1);
+            cd(crrDir)
+        case '.mat'
+            fprintf('.mat file recognized\n')
+            if contains(baseName,'analysis')
+                fprintf('The analysis file is not meant to be imported\n')
+                baseName = strsplit(baseName,'analysis');
+                baseName = baseName{1};
+            end
+            fprintf('Importing %s...\n',[baseName,flEx])
+        otherwise
+            fprintf('This input file is not recognized.')
+            return
+    end
+    mat2bin(fullfile(flDir,[baseName,'.mat']),fileOutput)
+else
+    fprintf('The input file was not found')
+end
 end
 
 function mat2bin(fileFullPath, fileOutput)
 if ~exist(fileOutput,'file')
     [foDir,foBaseName,foExt] = fileparts(fileOutput);
-    [fiDir,fiBaseName,~] = fileparts(fileFullPath);
     dataLoader = UMSDataLoader(fileFullPath);
     data = dataLoader.getDataMatrix;
     fs = dataLoader.SamplingFrequency;
@@ -56,14 +56,16 @@ if ~exist(fileOutput,'file')
     fsRow(1) = fs;
     data = [fsRow;data]';
     try
-    fid_out = fopen(fileOutput,'w');
-    fwrite(fid_out,data,'double')
-    fclose(fid_out);
+        if ~strcmp(foExt,'.bin')
+            fileOutput = fullfile(foDir,[foBaseName,'.bin']);
+        end
+        fid_out = fopen(fileOutput,'w');
+        fwrite(fid_out,data,'int16');
+        fclose(fid_out);
     catch
         disp('There was an error creating/writing the file')
     end
 else
-    ovwrt = input('The file exists already. Do you wish to overwrite it?','s');
-    if 
+    disp('The file exists already.') 
 end
 end
