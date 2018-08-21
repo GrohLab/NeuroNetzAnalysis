0c351cb4c5b252de8de38afb13498db10f0702a2
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2018-08-21 18:37:20 +0200

    Bug fixing

--- logicEngine.m
+++ logicEngine.m
@@ -3,12 +3,11 @@ function [tIdx, koIdx] = logicEngine(IDsignal)
 %remove an event out of the analysis. To add a time consideration is among
 %the next steps. Currently, the function only takes care of the appearence of the
 %considered events in the further analyses.
-%   The function accepts one input argument: the ID of the signals. 
-% Emilio Isa?as-Camacho GrohLabs 2018
+%   The function accepts one input argument: the ID of the signals.
+% Emilio Isaias-Camacho GrohLabs 2018
 
 tIdx = selectTrigger(IDsignal);
-koIdx = kickOutEvents(discreteStackSize - [sum(tIdx), 0, 0],...
-    IDsignal(~tIdx));
+koIdx = kickOutEvents(IDsignal(~tIdx));
 koSubs = find(koIdx);
 koSubs(koSubs >= find(tIdx)) = koSubs(koSubs >= find(tIdx)) + 1;
 koIdx = false(numel(IDsignal),1);
@@ -34,31 +33,29 @@ else
 end
 end
 
-function koIdx = kickOutEvents(discreteStackSize, IDsignal)
-if discreteStackSize(1) - 2 == numel(IDsignal)
-    koIdx = false(numel(IDsignal),1);
-    disp('User prompt: Selection of interesting discrete signals...')
-    [kickOutIndexes,iok] = listdlg(...
-        'PromptString','Select the interesting events:',...
-        'ListString',IDsignal,...
-        'SelectionMode','multiple',...
-        'CancelString','None',...
-        'OKString','OK',...
-        'Name','Selection of discrete signals',...
-        'ListSize',[160,15*numel(IDsignal)]);
-    if iok
-        koIdx(kickOutIndexes) = true;
-    else
-        
+function koIdx = kickOutEvents(IDsignal)
+koIdx = false(numel(IDsignal),1);
+disp('User prompt: Selection of interesting discrete signals...')
+[kickOutIndexes,iok] = listdlg(...
+    'PromptString','Select the interesting events:',...
+    'ListString',IDsignal,...
+    'SelectionMode','multiple',...
+    'CancelString','None',...
+    'OKString','OK',...
+    'Name','Selection of discrete signals',...
+    'ListSize',[160,15*numel(IDsignal)]);
+if iok
+    koIdx(kickOutIndexes) = true;
+else
+    
+end
+if ~isempty(kickOutIndexes)
+    disp('Thank you. You chose the following signals as interesting:')
+    for cid = kickOutIndexes
+        fprintf('%s ',IDsignal{cid})
     end
-    if ~isempty(kickOutIndexes)
-        disp('Thank you. You chose the following signals as interesting:')
-        for cid = kickOutIndexes
-            fprintf('%s ',IDsignal{cid})
-        end
-        fprintf('\n');
-    else
-        disp('No other event will be considered for further analysis')
-    end
+    fprintf('\n');
+else
+    disp('No other event will be considered for further analysis')
 end
 end
