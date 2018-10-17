9760015256491bbc568f386eb33eaa47318ed7e0
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2018-10-17 18:16:26 +0200

    Loading the configuration file
    The different data types of the configuration parameters are planned to be loaded by this function and passed to the stackPrototype script.

---
+++ loadConfigFile.m
@@ -0,0 +1,8 @@
+function [timeLapse, binSz, tIdx, koIdx, iIdx, windowArray] =...
+    loadConfigFile(fID)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
