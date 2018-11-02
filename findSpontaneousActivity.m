20c3e9f308e98dfbbf2c0d788cead244d14f188a
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2018-11-02 14:19:41 +0100

    Sketching the spontaneuous finder

---
+++ findSpontaneousActivity.m
@@ -0,0 +1,10 @@
+function [spontaneouStack] =...
+    findSpontaneousActivity(spT, discreteTraces, timeLapse)
+%FINDSPONTANEOUSACTIVITY returns the activity in which there is no active
+%conditioning variables in the experiment. It is structured in a stacked
+%form and a PSTH can be easily computed.
+%   More information will be given in the future
+
+
+end
+
