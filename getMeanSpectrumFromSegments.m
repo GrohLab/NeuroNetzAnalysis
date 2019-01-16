0c7192192cd51ac0c5a415db668e876d18aa9579
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2019-01-16 18:26:18 +0100

    Corrected a mathematical error
    Angle averaging doesn't exist. Instead I need to average complex vectors and compute their angle or ITPC afterwards

--- getMeanSpectrumFromSegments.m
+++ getMeanSpectrumFromSegments.m
@@ -8,8 +8,9 @@ for csw = 1:size(sponSubs,1)
     sponIdx = sonogramStruct.TimeAxis > tx(sponSubs(csw,1)) &...
         sonogramStruct.TimeAxis < tx(sponSubs(csw,2));
     sponSpect = sponSpect + mean(abs(sonogramStruct.SpectrumImage(:,sponIdx)),2)';
-    anglSpect = anglSpect + mean(angle(sonogramStruct.SpectrumImage(:,sponIdx)),2)';
-    IPCT = IPCT + abs(mean(exp(1i*angle(sonogramStruct.SpectrumImage(:,sponIdx))),2))';
+    % anglSpect = anglSpect + mean(angle(sonogramStruct.SpectrumImage(:,sponIdx)),2)';
+    cmpxAngl = cmpxAngl + mean(exp(1i.*angle(sonogramStruct.SpectrumImage(:,sponIdx))),2)';
+    % IPCT = IPCT + abs(mean(exp(1i*angle(sonogramStruct.SpectrumImage(:,sponIdx))),2))';
 end
 sponSpect = (sponSpect/csw) .* exp(1i * (anglSpect/csw));
 IPCT = IPCT / csw;
