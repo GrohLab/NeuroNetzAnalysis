a69d1a1e7f095fc41ae7194b89e5c11517d9c76d
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2019-01-16 18:26:41 +0100

    Modified the rest of the code according to the math correction

--- getMeanSpectrumFromSegments.m
+++ getMeanSpectrumFromSegments.m
@@ -1,9 +1,11 @@
-function [sponSpect,IPCT] = getMeanSpectrumFromSegments(silIdx, sonogramStruct, fs)
+function [sponSpect,ITPC] = getMeanSpectrumFromSegments(silIdx, sonogramStruct, fs)
 tx = (0:length(silIdx) - 1) * (1/fs);
 sponObj = StepWaveform(silIdx,fs);
 sponSubs = sponObj.Triggers;
 sponSpect = zeros(1,size(sonogramStruct.SpectrumImage,1),'single');
-anglSpect = sponSpect;
+% vectSpect = sponSpect;
+% partialITPC = anglSpect;
+cmpxAngl = sponSpect;
 for csw = 1:size(sponSubs,1)
     sponIdx = sonogramStruct.TimeAxis > tx(sponSubs(csw,1)) &...
         sonogramStruct.TimeAxis < tx(sponSubs(csw,2));
@@ -12,6 +14,6 @@ for csw = 1:size(sponSubs,1)
     cmpxAngl = cmpxAngl + mean(exp(1i.*angle(sonogramStruct.SpectrumImage(:,sponIdx))),2)';
     % IPCT = IPCT + abs(mean(exp(1i*angle(sonogramStruct.SpectrumImage(:,sponIdx))),2))';
 end
-sponSpect = (sponSpect/csw) .* exp(1i * (anglSpect/csw));
-IPCT = IPCT / csw;
+sponSpect = (sponSpect/csw) .* exp(1i * (angle(cmpxAngl/csw)));
+ITPC = abs(cmpxAngl/csw);
 end
