0e2b899ae9720144b11aed675533a87f6909041a
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2019-01-18 19:17:52 +0100

    New class and new function to finish the data explorer

--- saveConfigFile.m
+++ saveConfigFile.m
@@ -1,5 +1,4 @@
-function [iok] =...
-    saveConfigFile(conFiNa, timeLapse, binSz, tIdx, koIdx, iIdx, windowArray)
+function [iok] = saveConfigFile(
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 fID = fopen(conFiNa,'w');
