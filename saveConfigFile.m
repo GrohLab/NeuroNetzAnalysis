d7f947f8ad4be055a9be22d58c3882f452be195b
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2019-01-21 10:32:10 +0100

    Writing the configuration in a text file

--- saveConfigFile.m
+++ saveConfigFile.m
@@ -3,7 +3,9 @@ function [iok] = saveConfigFile(conFiNa, conFlags, sigNames)
 %   Detailed explanation goes here
 if size(conFlags,2) == length(sigNames)
     fID = fopen(conFiNa,'w');
-    
+    % Writing the trigger
+    fprintf(fID,'t:\t %s\n',sigNames{conFlags(:,1)});
+    % Writing the 
 else
     fprintf('Please verify the inputs to the function.\n')
     fprintf('The length of the configuration flag and the signal names ')
