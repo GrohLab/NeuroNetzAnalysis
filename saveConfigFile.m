ddf1aa46ae7f5c1a5fa6b44589f42a4189303328
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2019-01-21 10:04:21 +0100

    This has nothing to do with the 2DFreqAnalysis

--- saveConfigFile.m
+++ saveConfigFile.m
@@ -1,7 +1,15 @@
-function [iok] = saveConfigFile(
+function [iok] = saveConfigFile(conFiNa, conFlags, sigNames)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-fID = fopen(conFiNa,'w');
+if size(conFlags,2) == length(sigNames)
+    fID = fopen(conFiNa,'w');
+    
+else
+    fprintf('Please verify the inputs to the function.\n')
+    fprintf('The length of the configuration flag and the signal names ')
+    fprintf('differ.\n')
+    iok = -1;
+end
 
 end
 
