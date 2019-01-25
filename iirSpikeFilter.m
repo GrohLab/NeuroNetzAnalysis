6b6a615835ac9483636e9aaf310d05b27f586eb3
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2019-01-25 10:11:54 +0100

    Function to filter given signals to search for spikes

---
+++ iirSpikeFilter.m
@@ -0,0 +1,21 @@
+function [csig] = iirSpikeFilter(signal,sampling_frequency,cutFreq)
+%IIRSPIKEFILTER returns the filtered signal according to either the user
+%input in cutFreq or uses the default 600 - 9 kHz cut frequencies for the
+%band pass. 
+%   signal contains a broadband frequency spectra
+%   sampling_frequency has a sensible name
+%   cutFreq is a vector containing the low and high cut frequencies
+[b, a] = cheby2(3,30,1/sqrt(2));
+if ~(exist('cutFreq','var') && ~isempty(cutFreq) && length(cutFreq) == 2 &&...
+        cutFreq(1) > cutFreq(2) && diff(cutFreq) > 1e3)
+    cutFreq = [600, 9e3];
+end
+wo = (2*mean(cutFreq))/sampling_frequency;
+wc = (2*cutFreq)./sampling_frequency;
+fprintf('Spike filter set at: [%.2f, %.2f]\n',cutFreq(1),cutFreq(2))
+[num, den] = iirlp2bp(b, a, wo, wc);
+fprintf('Filtering the signal...')
+csig = filtfilt(num, den, signal);
+fprintf(' done!\n');
+end
+
