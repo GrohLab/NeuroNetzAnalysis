479f4a7e9d27fcfea08262d8800fe3b7fe043eee
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2019-01-29 14:34:26 +0100

    Slight display optimization
    I should read the blogs from the MATLAB expert on how to display large datasets efficiently

--- PlottingFunctions/plotEEGchannels.m
+++ PlottingFunctions/plotEEGchannels.m
@@ -1,23 +1,35 @@
 function fhand = plotEEGchannels(EEG, labels, duration, fs, scale)
 fhand = figure('Name','EEG');hold on
 title('EEG channels')
-ch=size(EEG,1);
+[Nch, Ns] = size(EEG);
 step = 5*std(EEG(:));
-offset = step * ch;
-N = size(EEG,2);
-timeS = (0:N-1)*(1/fs);
-tick = zeros(1,ch,'single');
-for c = 1:ch
+offset = step * Nch;
+if fs > 1e9
+    fprintf('Attempting a downsample of the signals only for displaying')
+    fprintf(' purposes to 1 kHz\n')
+    dFs = 1e3;
+    DSfactor = round(fs/dFs);
+    EEG = downsample(EEG',DSfactor)';
+    Ns = size(EEG,2);
+    dt = 1/dFs;
+else
+    dFs = fs;
+    dt = 1/fs;
+end
+timeS = (0:Ns-1) * dt;
+tick = zeros(1,Nch,'single');
+tmSub = round(duration * dFs);
+for c = 1:Nch
     tick(c) = offset;
-    plot(timeS(1:1+duration*fs),...
-        scale*EEG(c,1:1+duration*fs)+offset,'b')
+    plot(timeS(1:tmSub),...
+        scale*EEG(c,1:tmSub)+offset,'b')
     offset = offset-step;
 end
-axis([0, timeS(1+duration*fs),...
-    tick(ch)-2*step, tick(1)+2*step])
+axis([0, timeS(tmSub),...
+    tick(Nch)-2*step, tick(1)+2*step])
 ylabel('Channels')
 xlabel('Time [s]')
 labels(strcmp(labels,'A1'))=[];
 labels(strcmp(labels,'A2'))=[];
-set(gca,'YTick',sort(tick),'YTickLabel',labels(ch:-1:1))
+set(gca,'YTick',sort(tick),'YTickLabel',labels(Nch:-1:1))
 end
