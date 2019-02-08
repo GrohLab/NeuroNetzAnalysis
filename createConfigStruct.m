01ac9a31d675e9d51931126ca216bb7505aaf003
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2019-02-08 09:45:09 +0100

    Creating configuration structure
    Analyzing a population of juxtasomal recordings

--- createConfigStruct.m
+++ createConfigStruct.m
@@ -1,14 +1,15 @@
-function [configStruct] = createConfigStruct(EphysDir)
+function [configStruct, expFileNames] = createConfigStruct(EphysDir)
 %CREATECOFIGSTRUCT returns a structure with the information required to
 %proceed with a population analysis.
 %   Prompting the user for specific inputs. For example, the cell type can
 %   be red from the RecDB variable. However, the conditional variables or
 %   signals need to be assumed and hard-coded in this function. 
-dbTabFile = fullfile(EphysDir,'ephys_database.mat');
+% Emilio Isaías-Camacho @GrohLab 2019
+dbTabFilePath = fullfile(EphysDir,'ephys_database.mat');
 deleteLastCell = @(x) x(1:end-1);
-if exist(dbTabFile,'file')
+if exist(dbTabFilePath,'file')
     anMatDir = fullfile(EphysDir,'EphysData','AnalysisMatFiles');
-    load(dbTabFile,'RecDB')
+    load(dbTabFilePath,'RecDB')
     Nx = size(RecDB,1);
     cellTypes = unique(RecDB.PhysioNucleus);
     %% Cell type selection
@@ -123,9 +124,11 @@ if exist(dbTabFile,'file')
         'Name','Selection of exclude signals',...
         'ListSize',[160,15*numel(exclVars)],'InitialValue',1);
     if iok
-        configStruct.Exclude = exclVars(exclSub);
+        % Exclude and grooming should ALWAYS be excluded.
+        configStruct.Exclude = cat(1,...
+            exclVars(exclSub),{'exclude';'grooming'});
     else
-        fprintf('No signal will be excluded from the experiment')
+        fprintf('No signal will be excluded from the experiment.\n')
         configStruct.Exclude = [];
     end
     %% Ignore selection
@@ -133,6 +136,7 @@ if exist(dbTabFile,'file')
     eIdx(exclSub) = true;
     ignVars = exclVars(~eIdx);
     configStruct.Ignore = [];
+    ignSub = [];
     if ~isempty(ignVars)
         [ignSub, iok] = listdlg('ListString',ignVars,...
             'PromptString','Select those signals to ignore:',...
@@ -144,16 +148,57 @@ if exist(dbTabFile,'file')
             'ListSize',[160,15*numel(exclVars)],'InitialValue',1);
         if iok
             configStruct.Ignore = ignVars(ignSub);
+        else
+            fprintf('No signal will be ignored.\n')
         end
     end
     %% Conditioning Window selection
     % Include those signals which were not excluded, not ignored, and not
-    % being the trigger. Obviously
-    ignVars(ignSub)
-    exclVars(exclSub)
-    condVars(triggerIdx)
+    % being the trigger. Obviously.
+    % I am taking the ignVars because it contains the variables without the
+    % trigger, the excluded and, of course, the ignored signals. 
+    if ~isempty(ignVars)
+        vwIdx = false(numel(ignVars),1);
+        vwIdx(ignSub) = true;
+        vwVars = ignVars(~vwIdx);
+        dfltVals =repmat(...
+            {[num2str(-timeLapse(1)),', ',num2str(timeLapse(2))]},...
+            numel(ignVars),1);
+        vwFlag = false;
+        if ~isempty(vwVars)
+            fstr = cat(2,vwVars{1},' (-time before, +time after) [ms]:');
+            vwVars(1) = {fstr};
+            windowTimes = inputdlg(vwVars,...
+                'Delay for the interesting signals',...
+                [1,20],...
+                dfltVals(~vwIdx));
+            windowArray =...
+                cell2mat(...
+                cellfun(@str2num, windowTimes, 'UniformOutput', false)); 
+            vwFlag = true; 
+        end
+        dfltVals = cell2mat(...
+            cellfun(@str2num, dfltVals, 'UniformOutput', false));
+        if vwFlag
+            dfltVals(~vwIdx,:) = windowArray;
+        end
+        auxStruct = struct('Name','name','Window',[0,0]);
+        auxStruct = repmat(auxStruct,numel(ignVars),1);
+        for cvw = 1:numel(ignVars)
+            auxStruct(cvw).Name = ignVars(cvw);
+            auxStruct(cvw).Window = dfltVals(cvw,:)*1e-3;
+        end
+        configStruct.ConditionWindow = auxStruct;
+    else
+        % Empty format for no conditioning window. 'none' name and no time
+        % before, nor after.
+        configStruct.ConditionWindow = struct('Name','none','Window',[0,0]);
+    end
 else
-    
+    fprintf('The given file does not exist. ')
+    fprintf('Please check the name and the path.\n')
+    fprintf('%s',dbTabFilePath)
+    configStruct = [];
 end
 
 end
