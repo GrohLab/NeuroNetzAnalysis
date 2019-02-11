6cc028964f06c265112d44b88a8a194a5079faa0
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2019-02-11 18:22:19 +0100

    Implemented the experiment triggered stacks

--- signal_creTriggerase.m
+++ signal_creTriggerase.m
@@ -1,4 +1,4 @@
-function [discreteStack,continuouStack] =...
+function [dStruct, cStruct] =...
     signal_creTriggerase(configStruct,analysisFilePath)
 %SIGNAL_TRIGGERASE cuts the signals in the analysis file specified by
 %_analysisFileName_ given the trigger signal in the configuration structure
@@ -6,14 +6,18 @@ function [discreteStack,continuouStack] =...
 %    This function does not need the exclusion nor the neglection
 %    information contained in the configuration structure; it creates the
 %    stacks regardless of this information.
+
+% Helpful function handlers 
+expRng = @(x) x(1):x(2);
+
 %% Validation of the structure
 if isempty(configStruct) || isempty(configStruct.Trigger) ||...
         isempty(configStruct.Trigger.Name)
     fprintf('The stack cannot be built without a trigger.\n')
     fprintf('Please check this issue and try again creating a valid ')
     fprintf('configuration structure.\n')
-    discreteStack = [];
-    continuouStack = [];
+    dStruct = [];
+    cStruct = [];
     return
 end
 %% Loading and acconditioning data from the analysis file. NOT GENERALIZED
@@ -47,15 +51,17 @@ if ~isempty(Sig)
     Sig = Sig.Triggers;
     sigIDs = fieldnames(Sig);
     tnIdx = strcmpi(sigIDs,configStruct.Trigger.Name);
-    twfObj = StepWaveform(Sig.(sigIDs(tnIdx)),fs);
+    twfObj = StepWaveform(Sig.(sigIDs{tnIdx}),fs);
     tSubs = twfObj.Triggers;
     dsIdx = structfun(@islogical,Sig);
     Nds = sum(dsIdx);
     lenFlds = structfun(@length,Sig);
-    discEvents = false(Nds,mean(lenFlds(dsIdx)));
+    discEvents = false(Nds,ceil(mean(lenFlds(dsIdx))));
+    cds = 1;
     for cf = 1:numel(sigIDs)
-        if islogical(Sig.(sigIDs(cf)))
-            discEvents(cf) = Sig.(sigIDs(cf));
+        if islogical(Sig.(sigIDs{cf}))
+            discEvents(cds,:) = Sig.(sigIDs{cf});
+            cds = cds + 1;
         end
     end
 else
@@ -71,10 +77,22 @@ else
         discEvents(cf,:) = StepWaveform.subs2idx(Sig(cf).Triggers,Ns);
     end
 end
+ndName = sigIDs{~dsIdx};
 sigIDs(~dsIdx) = [];
 sigIDs = cat(isrow(sigIDs)*2 + iscolumn(sigIDs)*1,{'spikes'},sigIDs);
 discEvents = cat(1,StepWaveform.subs2idx(spSubs,size(discEvents,2)),...
     discEvents);
+discEvents = [discEvents(strcmpi(sigIDs,configStruct.Trigger.Name),:);...
+    discEvents];
+discEvents([false;strcmpi(sigIDs,configStruct.Trigger.Name)],:) = [];
+sigIDs = cat(isrow(sigIDs)*2 + iscolumn(sigIDs)*1,...
+    {configStruct.Trigger.Name},sigIDs);
+sigIDs(...
+    cat(isrow(sigIDs)*2 + iscolumn(sigIDs)*1,...
+    false,...
+    strcmpi(sigIDs(2:end),configStruct.Trigger.Name))) = [];
+
+
 % Loading the local field potential signal (sampled at 1 kHz for Toni's
 % data). There is no alternative if this variable is missing.
 LFP = load(analysisFilePath,'EEG');
@@ -85,15 +103,15 @@ else
     fs = repmat(fs,2,1);
 end
 % Get the whisking signal
-wIdx = strcmpi(sigIDs,'whisking');
-if numel(Sig) > 1
-    WHI = Sig.(sigIDs(wIdx));
+if numel(Sig) == 1
+    WHI = Sig.(ndName);
 else
     fprintf('It is unclear where to take the whisker signal from.\n')
     WHI = zeros(size(LFP.data),'single');
 end
 contEvents = cat(1,LFP.data,WHI);
 conIDs = {'LFP','Whisking'};
+NsC = length(contEvents);
 
 %% Construct stacks: Initialization
 %   Preallocation of the discrete stack:
@@ -115,13 +133,53 @@ postSamplesC = ceil(timeSpan(2) * fs(2));
 NtC = prevSamplesC + postSamplesC + 1;
 % Total number of continuous conditioning variables.
 NeC = size(contEvents,1);
+tSubsC = round(tSubs .* (fs(2)/fs(1)));
 continuouStack = zeros(NeC,NtC,Na);
 
 %% Construct stacks: cre_Triggerase
-
 for cap = 1:size(tSubs,1)
-    
+    onf = configStruct.Trigger.Edge*1 + ~configStruct.Trigger.Edge*2;
+    ctSubs = tSubs(cap,onf);
+    ctSubsC = tSubsC(cap,onf);
+    segSubs = [ctSubs - prevSamples, ctSubs + postSamples];
+    normSubs = segSubs;
+    if segSubs(1) < 1
+        printf('The viewing window is out of the signal range.\n')
+        fprintf('%d samples required before the signal. Omitting...\n',...
+            -segSubs(1))
+        segSubs(1) = 1;
+        normSubs = Nt - segSubs(2) + 1:Nt;
+    elseif segSubs(2) > Ns
+        fprintf('The viewing window is out of the signal range.\n')
+        fprintf('%d samples required after the signal. Omitting...\n',...
+            segSubs(2))
+        segSubs(2) = Ns;
+        normSubs = 1:diff(segSubs)+1;
+    end
+    if diff(fs) %% Different signal lengths
+        segSubsC = [ctSubsC - prevSamplesC, ctSubsC + postSamplesC];
+        normSubsC = segSubsC;
+        if segSubsC(1) < 1
+            printf('The viewing window is out of the signal range.\n')
+            fprintf('%d samples required before the signal. Omitting...\n',...
+                -segSubsC(1))
+            segSubsC(1) = 1;
+            normSubsC = NtC - segSubsC(2) + 1:NtC;
+        elseif segSubsC(2) > NsC
+            fprintf('The viewing window is out of the signal range.\n')
+            fprintf('%d samples required after the signal. Omitting...\n',...
+                segSubsC(2))
+            segSubsC(2) = NsC;
+            normSubsC = 1:diff(segSubsC)+1;
+        end
+    else
+        segSubsC = segSubs;
+        normSubsC = normSubs;
+    end
 end
-
-
+discreteStack(:,expRng(normSubs),cap) = discEvents(:,expRng(segSubs));
+continuouStack(:,expRng(normSubsC),cap) = contEvents(:,expRng(segSubsC));
+%% Output structures:
+dStruct = struct('Stack',discreteStack,'SignalIDs',sigIDs);
+cStruct = struct('Stack',continuouStack,'SignalIDs',conIDs);
 end
