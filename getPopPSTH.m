a0f3ba640eaa14fd0c8b0036b61a7616cf1d66a5
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2019-02-26 18:42:52 +0100

    Preparations for the generalization

--- getPopPSTH.m
+++ getPopPSTH.m
@@ -8,8 +8,8 @@ function [psthStr] = getPopPSTH(dSt,condSt,configStruct)
 % Gathering the necessary information to compute the PSTH combinations
 % according to the user selection.
 [Nv, Nts, Nap] = size(dSt.Stack);
-fs = condSt.SamplingFrequencies(1); 
-Ncv = size(condStr.CVDFlags,1); % Number of conditioning variables
+fs = configStruct.SamplingFrequencies(1); 
+Ncv = size(condSt.CVDFlags,1); % Number of conditioning variables
 finishedButton = false;
 % To have a pure triggered PSTH (only taking random occurances of the
 % ignored variables) we need to take those trials which were NOT excluded
@@ -17,23 +17,29 @@ finishedButton = false;
 % window of interest. De Morgan law: ~a & ~b = ~(a | b)
 cleanIdx = ~(condSt.ExcludeFlags | any(condSt.CVDFlags,1));
 cl_swaps = sum(cleanIdx);
-fprintf('Percentage of ''clean'' trials: %.2f (%d/%d)\n',cl_swaps/Nap,...
+fprintf('Percentage of ''clean'' trials: %.2f%% (%d/%d)\n',cl_swaps/Nap,...
     cl_swaps, Nap)
-cleanPSTH = sum(dst.Stack(:,:,cleanIdx),3)./cl_swaps;
-
+cleanPSTH = sum(dSt.Stack(:,:,cleanIdx),3)./cl_swaps;
+tx = (0:Nts-1) * 1/fs - configStruct.ViewWindow(1);
+binWidth = ceil(configStruct.BinSize * fs);
+Nbin = ceil(Nts/binWidth);
+btx = (0:Nbin-1) * configStruct.BinSize - configStruct.ViewWindow(1);
 % If the user selected 1 conditioning variable, then there are no more
 % possible combinations than the clean trigger and the trigger plus the
 % conditioning variable.
 if Ncv == 1
-    uniquePSTH = sum(dst.Stack(:,:,~condSt.ExcludeFlags &...
-        any(condSt.CVDFlags,1)),3);
-else
+    inTrials = ~condSt.ExcludeFlags & condSt.CVDFlags;
+    uniquePSTH = sum(dSt.Stack(:,:,inTrials),3)/sum(inTrials);
+    psthAux = binTime(uniquePSTH(2:Nv,:),configStruct.BinSize,fs);
+elseif Ncv > 1
     % Otherwise, the function will prompt the user to select a combination
     % of triggers in a list dialog for a later plotting.
     
     while ~finishedButton
         
     end
+else
+    % This means no conditioning variable. The PSTH results to be only a 
 end
 
 end
