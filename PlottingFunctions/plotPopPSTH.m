eb68296a2a685a0d7652144eb30583c33f952be9
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2019-03-04 18:10:56 +0100

    Step into plotting the PSTHs

--- PlottingFunctions/plotPopPSTH.m
+++ PlottingFunctions/plotPopPSTH.m
@@ -8,40 +8,82 @@ function ax = plotPopPSTH(PSTHStruct, varargin)
 %% Input validation
 if isstruct(PSTHStruct) && isfield(PSTHStruct,'TimeAxis')
     p = inputParser;
-    defaultFigure = 'new';
-    validFigure = {'new','current','given'};
-    checkFigure = @(x) any(validatestring(x,validFigure));
-    
-    addRequired(p,'PSTHStruct',@isstruct);
+    % Bin size validation
     defaultBin = PSTHStruct.BinSize;
     small_limit = mean(diff(PSTHStruct.TimeAxis));
     attributes = {'scalar','>',small_limit,...
-        '<=', sum(abs([PSTHStruct.TimeAxis(1),PSTHStruct.TimeAxis(end)]))*0.01};
+        '<=', sum(abs([PSTHStruct.TimeAxis(1),PSTHStruct.TimeAxis(end)]))*0.1};
     addOptional(p,'BinSize', defaultBin, @(x)validateattributes(x,...
         {'numeric'},attributes));
-    addOptional(p,'Figure',defaultFigure, checkFigure);
+    % Structure validation
+    addRequired(p,'PSTHStruct',@isstruct);
+    % Display (figure)
+    figchk = 'matlab.ui.Figure';
+    defaultFig = figure('Visible','off',...
+        'Color',[1,1,1],...
+        'Name','Population PSTH');
+    addOptional(p,'Figure',defaultFig,@(x)isa(x,figchk));
+    % Actual validation
     p.parse(PSTHStruct, varargin{:})
 else
     fprintf('The PSTH structure is either corrupt or hasn''t been created\n')
     return
 end
 %% Initialization
+% Bin size
 binsize = p.Results.BinSize;
+ttx = PSTHStruct.TimeAxis;
+btx = ttx(1):binsize:ttx(end);
+% Figure
+fig = p.Results.Figure;
+fig.Children = axes();
 % Number of conditions combinations
 Ncc = length(PSTHStruct.PSTHs);
 % Figure organization
-if sqrt(Ncc) - fix(sqrt(Ncc)) == 0
-    Nrow = sqrt(Ncc);
+st = sqrt(Ncc);
+if st - fix(st) == 0
+    Nrow = st;
     Ncol = Nrow;
-elseif ~mod(Ncc,2)
+elseif ~mod(Ncc,2) && Ncc/2 <= 5
     Nrow = 2;
     Ncol = Ncc/Nrow;
-elseif ~mod(Ncc,3)
+elseif ~mod(Ncc,3) && Ncc/3 <= 5
     Nrow = 3;
     Ncol = Ncc/Nrow;
+else
+    Ncol = 5;
+    Nrow = ceil(Ncc/Ncol);
 end
 
-
+%% Plotting procedure
+ax = gobjects(Ncc,1);
+[Nv, Ns, Nx] = size(PSTHStruct.PSTHs(1).PSTHstack);
+fs = 1/mean(diff(ttx));
+cmap = jet(Ncc);
+fig.Visible = 'on';
+for csp = 1:Ncc
+    ax(csp) = subplot(Nrow,Ncol,csp,fig.Children);
+    hold(ax(csp),'on')
+    for cexp = Nx:-1:1
+        % Binning the signals using the _binsize_ variable
+        [bPSTH, binWidth] =...
+            binTime(PSTHStruct.PSTHs(csp).PSTHstack(2:Nv,:,cexp),binsize,fs);
+        % Removing those variables which didn't occure at all
+        emptyFlag = sum(bPSTH,1) == 0;
+        bPSTH = bPSTH(:,~emptyFlag);
+        nNv = size(bPSTH,2);
+        lbs = PSTHStruct.SignalIDs([false,~emptyFlag]);
+        cSubs = find([false,~emptyFlag]);
+        for cv = 1:nNv
+            bar(ax(csp),btx,(cexp+1)*bPSTH(:,cv),'EdgeColor','none',...
+                'FaceAlpha',0.3,'FaceColor',cmap(cSubs(cv),:),...
+                'DisplayName',lbs{cv},'BaseValue',cexp)
+        end
+        plot(ax(csp),ttx,PSTHStruct.PSTHs(csp).PSTHstack(1,:,cexp),...
+            'Color',cmap(1,:),'DisplayName',PSTHStruct.Trigger)
+        legend show
+    end
+end
 end
 
 % p = inputParser;
