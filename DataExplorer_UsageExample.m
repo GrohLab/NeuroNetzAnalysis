c94434ac219a35c87e2c6a5b95fd89184eb6a58a
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2019-03-29 18:49:32 +0100

    Tiny modifications for a running script

--- DataExplorer_UsageExample.m
+++ DataExplorer_UsageExample.m
@@ -2,6 +2,7 @@ MrToniDBPath = 'E:\Database\EphysData';
 import2stack(MrToniDBPath)
 [configStruct, expFile] = createConfigStruct(getParentDir(MrToniDBPath,1));
 savePopConfigFile('ToniExample.gcf',configStruct)
-[dPopStruct, cPopStruct, conditionStruct, configStruct] = getPopulationStack(getParentDir(MrToniDBPath,1));
+[dPopStruct, cPopStruct, conditionStruct, configStruct] =...
+    getPopulationStack(getParentDir(MrToniDBPath,1));
 PSTHStruct = getPopPSTH(dPopStruct,conditionStruct,configStruct);
-plotPopPSTH(PSTHStruct,'BinSize',0.015,'PlotStyle','middle')
+plotPopPSTH(PSTHStruct,'PlotStyle','simple')
