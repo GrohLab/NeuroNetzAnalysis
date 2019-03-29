7d76165704af870bf85161a3c9f9efcab8f3b0d9
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2019-03-29 10:15:09 +0100

    Binning time in seconds

--- DataExplorer_UsageExample.m
+++ DataExplorer_UsageExample.m
@@ -4,4 +4,4 @@ import2stack(MrToniDBPath)
 savePopConfigFile('ToniExample.gcf',configStruct)
 [dPopStruct, cPopStruct, conditionStruct, configStruct] = getPopulationStack(getParentDir(MrToniDBPath,1));
 PSTHStruct = getPopPSTH(dPopStruct,conditionStruct,configStruct);
-plotPopPSTH(PSTHStruct,'BinSize',15,'PlotStyle','middle')
+plotPopPSTH(PSTHStruct,'BinSize',0.015,'PlotStyle','middle')
