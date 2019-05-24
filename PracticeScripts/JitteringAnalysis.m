af6799ad1f7fc04eee289f0a75021af0c53d447a
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2019-05-24 17:46:12 +0200

    Included a saving option for the spike sorted data

--- PracticeScripts/JitteringAnalysis.m
+++ PracticeScripts/JitteringAnalysis.m
@@ -1,5 +1,6 @@
 %% Constant values
 % Prefixes
+anaFlag = false;
 k = 1e3; % Kilo
 m = 1e-3; % milli
 zs = @(x)mean(x)/std(x);
@@ -7,11 +8,12 @@ addFst = @(x,y)cat(find(size(x)~=1),y,x);
 addLst = @(x,y)cat(find(size(x)~=1),x,y);
 uninterestingVals = @(x)~(isempty(x) || isnan(x) || isinf(x) || x == 0);
 printFig = @(x,fname) set(x,'RendererMode','manual','Renderer','painters',...
-        'PaperOrientation','landscape','Name',fname);
+    'PaperOrientation','landscape','Name',fname);
 %% Select data file
 defaultPath = 'E:\Data\Jittering'; % Path where you would like to open a windows explorer window to select your data file
 [fileName, filePath] =...
-    uigetfile({'*.mat';'*.smr';'*.smrx'},'Select a data file',defaultPath);
+    uigetfile({'*analysis.mat';'*.mat';'*.smr';'*.smrx'},...
+    'Select a data file',defaultPath);
 if fileName == 0
     fprintf(1,'Cancel button pressed. See you next time!\n')
     return
@@ -29,154 +31,177 @@ switch fileExt
             fprintf(1,'No need to import, a mat file exists already\n')
         end
         fileName = strrep(fileName,fileExt,'.mat');
-    case '.mat'
+    case {'.mat','analysis.mat'}
         fprintf(1,'\nMatlab file selected.\n')
+        anaFlag = any(strfind(fileName,'analysis.mat'));
+        if anaFlag
+            fprintf(1,'Skipping to the plotting section... \n')
+        end
     otherwise
         fprintf(1,'\nFile type not recognized. Please provide a valide file\n')
         return
 end
-%% Loading the variables to extract all triggers
-% Loading the file into a structure
-varsInFile = load(fullfile(filePath,fileName));
-fNames = fieldnames(varsInFile);
-chanIdx = startsWith(fNames,'chan');
-headIdx = startsWith(fNames,'head');
-headSub = find(headIdx);
-fsArray = zeros(1,nnz(headIdx));
-for chead = 1:nnz(headIdx)
-    fsArray(chead) = 1e6/(varsInFile.(fNames{headSub(chead)}).sampleinterval);
-    newField = erase(varsInFile.(fNames{headSub(chead)}).title,' ');
-    data.(newField) = varsInFile.(strrep(fNames{headSub(chead)},'head','chan'));
-end
-Ns = mode(structfun(@length,data));
-fs = mode(fsArray);
-fprintf(1,'Sampling frequency: %.2f Hz\n',fs)
 
-IDsignal = fieldnames(data);
-% Prompting for the triggers (light/laser and piezo pulses)
-idx = false(numel(fieldnames(data)),1);
-[triggerIdx, iok] = listdlg(...
-    'PromptString','Select all trigger signals:',...
-    'ListString',IDsignal,...
-    'SelectionMode','multiple',...
-    'CancelString','Cancel',...
-    'OKString','OK',...
-    'Name','Triggers',...
-    'ListSize',[160,15*numel(IDsignal)]);
-if ~iok
-    disp('You can always checking the file in Spike2. See you next time!')
-    clearvars
-    return
-end
-idx(triggerIdx) = true;
-
-% Prompting for the signal(s)
-[signalIdx, iok] = listdlg(...
-    'PromptString','Select all signals:',...
-    'ListString',IDsignal,...
-    'SelectionMode','multiple',...
-    'CancelString','Cancel',...
-    'OKString','OK',...
-    'Name','Triggers',...
-    'ListSize',[160,15*numel(IDsignal)]);
-if ~iok
-    disp('You can always checking the file in Spike2. See you next time!')
-    clearvars
-    return
-end
-IDcontinuous = IDsignal;
-IDcontinuous = IDcontinuous(signalIdx);
-for ccd = 1:numel(IDcontinuous)
-    ContinuousData.(IDcontinuous{ccd}) = data.(IDcontinuous{ccd});
+if ~anaFlag
+    [~,~,fExt] = fileparts(fileName);
+    afn = fullfile(filePath,[erase(fileName,fExt),'_analysis.mat']);
 end
 
-% Extracting the timepoints of the step functions
-cellTriggers = cell(1,numel(triggerIdx));
-tSub = 1;
-for cts = triggerIdx
-    auxWf = StepWaveform(data.(IDsignal{cts}),fs,'Volts',IDsignal{cts});
-    auxArray = auxWf.Triggers;
-    if isempty(auxArray)
-        % If the triggers are empty, the loop jumps to the next selected
-        % trigger.
-        cellTriggers(tSub) = [];
-        fprintf(1,'%s skipped!\n',IDsignal{cts})
-        idx(cts) = false;
-        triggerIdx(tSub) = [];
-        continue
+if ~anaFlag
+    %% Loading the variables to extract all triggers
+    % Loading the file into a structure
+    varsInFile = load(fullfile(filePath,fileName));
+    fNames = fieldnames(varsInFile);
+    chanIdx = startsWith(fNames,'chan');
+    headIdx = startsWith(fNames,'head');
+    headSub = find(headIdx);
+    fsArray = zeros(1,nnz(headIdx));
+    for chead = 1:nnz(headIdx)
+        fsArray(chead) = 1e6/(varsInFile.(fNames{headSub(chead)}).sampleinterval);
+        newField = erase(varsInFile.(fNames{headSub(chead)}).title,' ');
+        data.(newField) = varsInFile.(strrep(fNames{headSub(chead)},'head','chan'));
     end
-    Triggers.(IDsignal{cts}) = StepWaveform.SCBrownMotion(auxArray);
-    cellTriggers(tSub) = {auxArray};
-    tSub = tSub + 1;
-end
-
-IdxTriggers = cell2struct(cellTriggers,IDsignal(triggerIdx),2);
-
-%% Condition search and construction
-% We know that the piezo stimulation is going upwards and downwards, and
-% the laser has delays with respect to the piezo onset and sometimes
-% frequency stimulus.
-
-% Piezo: Up and down and first pulse of a pulse train
-% condFig = figure('Visible','off','Color',[1,1,1]);
-
-upIdx = IdxTriggers.piezo(:,1).*data.piezo > 0;
-upSub = find(upIdx);
-upFst = StepWaveform.firstOfTrain(upSub/fs);
-dwIdx = IdxTriggers.piezo(:,2).*data.piezo < 0;
-dwSub = find(dwIdx);
-dwFst = StepWaveform.firstOfTrain(dwSub/fs);
-
-Conditions = struct('name','piezoTrainUp','Triggers',upSub(upFst));
-Conditions(2).name = 'piezoTrainDw';
-Conditions(2).Triggers = dwSub(dwFst);
-Conditions(3).name = 'piezoTrainUpandDw';
-Conditions(3).Triggers = union(upSub(upFst),dwSub(dwFst));
-Conditions(4).name = 'piezoAllUp';Conditions(4).Triggers = upSub;
-Conditions(5).name = 'piezoAllDw';Conditions(5).Triggers = dwSub;
-Conditions(6).name = 'piezoAll';Conditions(6).Triggers = union(upSub,dwSub);
-
-if isfield(IdxTriggers,'laser')
-    disp('Development...')
-    %     axes(condFig,'NextPlot','add');
-    lsSub = find(IdxTriggers.laser(:,1));
-    lsFst = StepWaveform.firstOfTrain(lsSub/fs, 5 - 1e-3);
-    Conditions(7).name = 'laserTrain'; Conditions(7).Triggers = lsSub(lsFst);
-    Conditions(8).name = 'laserAll'; Conditions(8).Triggers = lsSub;
-    if isempty(Conditions(7).Triggers)
-        Conditions(7) = [];
+    Ns = mode(structfun(@length,data));
+    fs = mode(fsArray);
+    fprintf(1,'Sampling frequency: %.2f Hz\n',fs)
+    
+    IDsignal = fieldnames(data);
+    % Prompting for the triggers (light/laser and piezo pulses)
+    idx = false(numel(fieldnames(data)),1);
+    [triggerIdx, iok] = listdlg(...
+        'PromptString','Select all trigger signals:',...
+        'ListString',IDsignal,...
+        'SelectionMode','multiple',...
+        'CancelString','Cancel',...
+        'OKString','OK',...
+        'Name','Triggers',...
+        'ListSize',[160,15*numel(IDsignal)]);
+    if ~iok
+        disp('You can always checking the file in Spike2. See you next time!')
+        clearvars
+        return
     end
-    if isempty(lsFst) || ~sum(lsFst)
-        timeDelay = abs(lsSub - Conditions(3).Triggers)/fs;
-    else
-        timeDelay = abs(lsSub(lsFst) - Conditions(3).Triggers)/fs;
+    idx(triggerIdx) = true;
+    
+    % Prompting for the signal(s)
+    [signalIdx, iok] = listdlg(...
+        'PromptString','Select all signals:',...
+        'ListString',IDsignal,...
+        'SelectionMode','multiple',...
+        'CancelString','Cancel',...
+        'OKString','OK',...
+        'Name','Triggers',...
+        'ListSize',[160,15*numel(IDsignal)]);
+    if ~iok
+        disp('You can always checking the file in Spike2. See you next time!')
+        clearvars
+        return
     end
-    delays = uniquetol(timeDelay,0.01);
-    Nc = numel(Conditions);
-    for ccond = numel(Conditions)+1:numel(Conditions)+numel(delays)
-        Conditions(ccond).name = sprintf('laserDelay_%d_ms',...
-            floor(k*delays(ccond-...
-            Nc)));
-        Conditions(ccond).Triggers = lsSub(ismembertol(timeDelay,...
-            delays(ccond - Nc),0.01));
+    IDcontinuous = IDsignal;
+    IDcontinuous = IDcontinuous(signalIdx);
+    for ccd = 1:numel(IDcontinuous)
+        ContinuousData.(IDcontinuous{ccd}) = data.(IDcontinuous{ccd});
     end
-end
-
-%% Spike finding
-spikeFindObj = UMSDataLoader(data.Spikes,fs);
-spikeFindObj.UMS2kPipeline;
-spkTms = spikeFindObj.SpikeTimes;
-UMSSpikeStruct = spikeFindObj.SpikeUMSStruct;
-spikeFindingData = struct('thresh',[],...
-    'minISI',UMSSpikeStruct.params.shadow,'spikes',spkTms,'ppms',fs/k,...
-    'timestamp',[]);
-if iscell(spkTms)
-    spT = cell(1,numel(spkTms));
-    for cn = 1:numel(spkTms)
-        spT(cn) = {StepWaveform.subs2idx(round(spkTms{cn}*fs),Ns)};
+    
+    % Extracting the timepoints of the step functions
+    cellTriggers = cell(1,numel(triggerIdx));
+    tSub = 1;
+    for cts = triggerIdx
+        auxWf = StepWaveform(data.(IDsignal{cts}),fs,'Volts',IDsignal{cts});
+        auxArray = auxWf.Triggers;
+        if isempty(auxArray)
+            % If the triggers are empty, the loop jumps to the next selected
+            % trigger.
+            cellTriggers(tSub) = [];
+            fprintf(1,'%s skipped!\n',IDsignal{cts})
+            idx(cts) = false;
+            triggerIdx(tSub) = [];
+            continue
+        end
+        Triggers.(IDsignal{cts}) = StepWaveform.SCBrownMotion(auxArray);
+        cellTriggers(tSub) = {auxArray};
+        tSub = tSub + 1;
     end
+    
+    IdxTriggers = cell2struct(cellTriggers,IDsignal(triggerIdx),2);
+    
+    %% Condition search and construction
+    % We know that the piezo stimulation is going upwards and downwards, and
+    % the laser has delays with respect to the piezo onset and sometimes
+    % frequency stimulus.
+    
+    % Piezo: Up and down and first pulse of a pulse train
+    % condFig = figure('Visible','off','Color',[1,1,1]);
+    
+    upIdx = IdxTriggers.piezo(:,1).*data.piezo > 0;
+    upSub = find(upIdx);
+    upFst = StepWaveform.firstOfTrain(upSub/fs);
+    dwIdx = IdxTriggers.piezo(:,2).*data.piezo < 0;
+    dwSub = find(dwIdx);
+    dwFst = StepWaveform.firstOfTrain(dwSub/fs);
+    
+    Conditions = struct('name','piezoTrainUp','Triggers',upSub(upFst));
+    Conditions(2).name = 'piezoTrainDw';
+    Conditions(2).Triggers = dwSub(dwFst);
+    Conditions(3).name = 'piezoTrainUpandDw';
+    Conditions(3).Triggers = union(upSub(upFst),dwSub(dwFst));
+    Conditions(4).name = 'piezoAllUp';Conditions(4).Triggers = upSub;
+    Conditions(5).name = 'piezoAllDw';Conditions(5).Triggers = dwSub;
+    Conditions(6).name = 'piezoAll';Conditions(6).Triggers = union(upSub,dwSub);
+    
+    if isfield(IdxTriggers,'laser')
+        disp('Development...')
+        %     axes(condFig,'NextPlot','add');
+        lsSub = find(IdxTriggers.laser(:,1));
+        lsFst = StepWaveform.firstOfTrain(lsSub/fs, 5 - 1e-3);
+        Conditions(7).name = 'laserTrain'; Conditions(7).Triggers = lsSub(lsFst);
+        Conditions(8).name = 'laserAll'; Conditions(8).Triggers = lsSub;
+        if isempty(Conditions(7).Triggers)
+            Conditions(7) = [];
+        end
+        if isempty(lsFst) || ~sum(lsFst)
+            timeDelay = abs(lsSub - Conditions(3).Triggers)/fs;
+        else
+            timeDelay = abs(lsSub(lsFst) - Conditions(3).Triggers)/fs;
+        end
+        delays = uniquetol(timeDelay,0.01);
+        Nc = numel(Conditions);
+        for ccond = numel(Conditions)+1:numel(Conditions)+numel(delays)
+            Conditions(ccond).name = sprintf('laserDelay_%d_ms',...
+                floor(k*delays(ccond-...
+                Nc)));
+            Conditions(ccond).Triggers = lsSub(ismembertol(timeDelay,...
+                delays(ccond - Nc),0.01));
+        end
+    end
+    
+    %% Spike finding
+    spikeFindObj = UMSDataLoader(data.Spikes,fs);
+    spikeFindObj.UMS2kPipeline;
+    spkTms = spikeFindObj.SpikeTimes;
+    UMSSpikeStruct = spikeFindObj.SpikeUMSStruct;
+    spikeFindingData = struct('thresh',[],...
+        'minISI',UMSSpikeStruct.params.shadow,'spikes',spkTms,'ppms',fs/k,...
+        'timestamp',[]);
+    %% Save *analysis.mat file
+    
+    Head = struct('FileInfo',fullfile(filePath,fileName),...
+        'IDs',IDsignal,'tSub',triggerIdx,'sSub',signalIdx);
+    save(afn,'Triggers','Conditions','UMSSpikeStruct','Head')
+    
 else
-    spT = {StepWaveform.subs2idx(round(spkTms*fs), Ns)};
+    varsInFile = load(fullfile(filePath,fileName));
+    UMSSpikeStruct = varsInFile.UMSSpikeStruct;
+    Triggers = varsInFile.Triggers;
+    
+    if iscell(spkTms)
+        spT = cell(1,numel(spkTms));
+        for cn = 1:numel(spkTms)
+            spT(cn) = {StepWaveform.subs2idx(round(spkTms{cn}*fs),Ns)};
+        end
+    else
+        spT = {StepWaveform.subs2idx(round(spkTms*fs), Ns)};
+    end
 end
 %%
 % Time window surrounding the trigger [time before, time after] in seconds
@@ -191,9 +216,12 @@ binSz(4:6,:) = repmat(0.5*m,3,1);
 %    getStacks(false(1,cols), ltOn, 'on', timeLapse * m, fs ,fs ,[],dataCell);
 ERASE_kIDX = false;
 clID = UMSDataLoader.getClustersID(UMSSpikeStruct,'good');
+subOffst = numel(clID) - 1;
 IDe = IDsignal(...
     ~cellfun(@strcmpi,IDsignal,repmat({'Spikes'},numel(IDsignal),1)));
-IDe = [cell([repmat('Cluster ',numel(clID),1),num2str(clID)]);IDe];
+IDe = [num2cell([repmat('Cluster ',numel(clID),1),num2str(clID)],2);IDe];
+ntSub = triggerIdx + subOffst;
+nsSub = signalIdx + subOffst;
 consideredSignalsIdx = false(size(IDe));
 OVW = false; % Overwrite figure flag
 for ccon = 1:numel(Conditions)
@@ -203,7 +231,7 @@ for ccon = 1:numel(Conditions)
         spT{cn},... Main neuron
         Conditions(ccon).Triggers, 'on',... Triggers, onset
         timeLapse(ccon,:), fs, fs,... Time lapse, and sampling frequencies
-        spT(othNeu),... Other discrete events in the 
+        spT(othNeu),... Other discrete events in the
         struct2cell(ContinuousData));
     Na = size(dSck,3);
     kIdx = false(1,Na);
@@ -220,14 +248,14 @@ for ccon = 1:numel(Conditions)
     set(psthFig.Children.Title,'String',[expName,' ',Conditions(ccon).name])
     printFig(psthFig,sprintf('PSTH for %s',expName))
     psthFigName = fullfile(filePath,[expName,'_PSTH_',Conditions(ccon).name,'.pdf']);
-    if ~exist(psthFigName,'file') || OVW 
+    if ~exist(psthFigName,'file') || OVW
         print(psthFig,psthFigName,'-dpdf','-bestfit')
     end
     rastFig = plotRaster(relativeSpikeTimes, timeLapse(ccon,:), fs,...
         [expName,' ',Conditions(ccon).name],IDe);
     printFig(rastFig,sprintf('Raster for %s',expName))
     rastFigName = fullfile(filePath,[expName,'_Raster_',Conditions(ccon).name,'.pdf']);
-    if ~exist(rastFigName,'file') || OVW 
+    if ~exist(rastFigName,'file') || OVW
         print(rastFig, rastFigName,'-dpdf','-bestfit')
     end
 end
