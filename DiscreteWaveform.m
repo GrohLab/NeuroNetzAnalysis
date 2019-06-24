c54c10310701d31d4f86b65e50ff2dbc54be4101
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2019-06-24 16:33:31 +0200

    Transferred the static methods to the Superclass
    From the StepWaveform class to the DiscreteWaveform Superclass

--- DiscreteWaveform.m
+++ DiscreteWaveform.m
@@ -11,11 +11,10 @@ classdef (Abstract) DiscreteWaveform < GeneralWaveform
         LastOfTrain
         Count
         Delta
-        
     end
     
     properties (Access = 'public')
-        MinIEI (1,1) = [];
+        MinIEI (1,1) = NaN; % Time in seconds
     end
     
     methods
@@ -34,12 +33,12 @@ classdef (Abstract) DiscreteWaveform < GeneralWaveform
             fs = obj.SamplingFreq;
             trigs = obj.Triggers;
             evntTms = trigs(:,1)./fs;
-            if ~isempty(obj.MinIEI)
-                fot = StepWaveform.firstOfTrain(evntTms,obj.MinIEI);
+            if ~isnan(obj.MinIEI)
+                fot = DiscreteWaveform.firstOfTrain(evntTms,obj.MinIEI);
             else
                 fprintf('The minimum inter-event interval MinIEI is not set!\n')
                 fprintf('The function will use the mean MinIEI...\n')
-                fot = StepWaveform.firstOfTrain(evntTms);
+                [fot, obj.MinIEI] = DiscreteWaveform.firstOfTrain(evntTms);
             end
         end
         
@@ -49,11 +48,11 @@ classdef (Abstract) DiscreteWaveform < GeneralWaveform
             trigs = obj.Triggers;
             evntTms = flip(trigs(:,1))./fs;
             if ~isempty(obj.MinIEI)
-                lot = flip(StepWaveform.firstOfTrain(evntTms,obj.MinIEI));
+                lot = flip(DiscreteWaveform.firstOfTrain(evntTms,obj.MinIEI));
             else
                 fprintf('The minimum inter-event interval MinIEI is not set!\n')
                 fprintf('The function will use the mean MinIEI...\n')
-                lot = flip(StepWaveform.firstOfTrain(evntTms));
+                [lot, obj.MinIEI] = flip(DiscreteWaveform.firstOfTrain(evntTms));
             end
         end
         
@@ -74,8 +73,71 @@ classdef (Abstract) DiscreteWaveform < GeneralWaveform
             leSubs = find(lot);
             d = abs(feSubs-leSubs)./obj.SamplingFreq;
         end
+    end
+    
+    %% Static public methods
+    methods (Static, Access = 'public')
+        % Recreate the signal with logic values.
+        function logicalTrace = subs2idx(subs,N)
+            if size(subs,2) == 2
+                logicalTrace = false(1,N);
+                for cmt = 1:size(subs,1)
+                    logicalTrace(subs(cmt,1):subs(cmt,2)) = true;
+                end
+            else
+                [Nr, Nc] = size(subs);
+                logicalTrace = false(Nr * (Nr < Nc) + Nc * (Nc < Nr),...
+                    N);
+                subsClass = class(subs);
+                switch subsClass
+                    case 'cell'
+                        for cmt = 1:size(subs,1)
+                            logicalTrace(cmt,subs{cmt}) = true;
+                        end
+                    case {'single','double'}
+                        logicalTrace(1,subs) = true;
+                    otherwise
+                        fprintf('Case not yet implemented...\n')
+                end
+            end
+        end
         
+        % Get a semi-logic trigger signal
+        function semiLogicSignal = SCBrownMotion(RaF)
+            [R,C] = size(RaF);
+            if ~any([R == 2,C == 2])
+                disp('What kind of rising and falling edges were given?')
+                semiLogicSignal = RaF;
+                return
+            end
+            if R > C
+                RaF = RaF';
+            end
+            semiLogicSignal = cumsum(RaF(1,:) - RaF(2,:));
+        end
         
+        % Get the first true value of a logic pulse
+        function [frstSpks, minIpi] = firstOfTrain(spkTimes, minIpi)
+            % OUTPUTS a logical index for the edges which are the first in
+            % time for the step pulse train.
+            Ipi = abs(diff(spkTimes));
+            if ~exist('minIpi','var')
+                minIpi = mean(Ipi);
+            end
+            Pks = Ipi < minIpi;
+            Sps = DiscreteWaveform.addFst(~Pks,true);
+            frstSpks = DiscreteWaveform.addLst(Sps(1:end-1) & Pks,false);
+        end 
+    end
+    
+    %% Static private methods
+    methods (Static, Access = 'private')
+        function new_array = addFst(array,element)
+            new_array = cat(find(size(array)~=1), element, array);
+        end
         
+        function new_array = addLst(array,element)
+            new_array = cat(find(size(array)~=1), array, element);
+        end
     end
 end
--- StepWaveform.m
+++ StepWaveform.m
@@ -145,69 +145,8 @@ classdef StepWaveform < DiscreteWaveform
                 edgeOut(repeatIdx) = false;
             end
         end
-        
-        function new_array = addFst(array,element)
-            new_array = cat(find(size(array)~=1), element, array);
-        end
-        
-        function new_array = addLst(array,element)
-            new_array = cat(find(size(array)~=1), array, element);
-        end
     end
     
-    %% Static public methods
-    methods (Static, Access = 'public')
-        % Recreate the signal with logic values.
-        function logicalTrace = subs2idx(subs,N)
-            if size(subs,2) == 2
-                logicalTrace = false(1,N);
-                for cmt = 1:size(subs,1)
-                    logicalTrace(subs(cmt,1):subs(cmt,2)) = true;
-                end
-            else
-                [Nr, Nc] = size(subs);
-                logicalTrace = false(Nr * (Nr < Nc) + Nc * (Nc < Nr),...
-                    N);
-                subsClass = class(subs);
-                switch subsClass
-                    case 'cell'
-                        for cmt = 1:size(subs,1)
-                            logicalTrace(cmt,subs{cmt}) = true;
-                        end
-                    case {'single','double'}
-                        logicalTrace(1,subs) = true;
-                    otherwise
-                        fprintf('Case not yet implemented...\n')
-                end
-            end
-        end
-        
-        % Get a semi-logic trigger signal
-        function semiLogicSignal = SCBrownMotion(RaF)
-            [R,C] = size(RaF);
-            if ~any([R == 2,C == 2])
-                disp('What kind of rising and falling edges were given?')
-                semiLogicSignal = RaF;
-                return
-            end
-            if R > C
-                RaF = RaF';
-            end
-            semiLogicSignal = cumsum(RaF(1,:) - RaF(2,:));
-        end
-        
-        % Get the first true value of a logic pulse
-        function frstSpks = firstOfTrain(spkTimes, minIpi)
-            % OUTPUTS a logical index for the edges which are the first in
-            % time for the step pulse train.
-            Ipi = abs(diff(spkTimes));
-            if ~exist('minIpi','var')
-                minIpi = mean(Ipi);
-            end
-            Pks = Ipi < minIpi;
-            Sps = StepWaveform.addFst(~Pks,true);
-            frstSpks = StepWaveform.addLst(Sps(1:end-1) & Pks,false);
-        end 
-    end
+    
 end
 
