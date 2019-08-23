51a2f3dd87fffac7e473fc1e6ff6e9318785666e
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2019-08-23 17:40:53 +0200

    New function to develop

---
+++ getLTPTriggers.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = getLTPTriggers(expFolder)
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
