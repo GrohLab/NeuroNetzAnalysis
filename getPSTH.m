c6aeafddb2a68a3e3bd8388ce592e8e74fc625d5
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2019-09-05 14:26:02 +0200

    Fixed the help function

--- getPSTH.m
+++ getPSTH.m
@@ -6,25 +6,26 @@ function [PSTH, trig, sweeps, timeAxis] =...
 % triggers found in the channel. 
 % 
 % [PSTH, triggerSummary, sweeps, timeaxis] =
-%     getPSTH(discreteStack, kickOutIdx, binSize (in seconds), samplingFrequency) 
+%     getPSTH(discreteStack, timeLapse, kIdx, binSz, fs) 
 %     
 % INPUTS:
 %     discreteStack - NxMxA array which contains the information about the
-%     discrete events occurring at the given trials
-%     kickOutIdx - is a 1xA logic array which serves as flag to exclude the
+%     discrete events occurring at the given trials.
+%     timeLapse - 1x2 array containing the time before and after the
+%     trigger to consider
+%     kIdx - is a 1xA logic array which serves as flag to exclude the
 %     selected trials.
-%     binSize - is an integer to bin the discrete events in a smaller time
+%     binSz - is an integer to bin the discrete events in a smaller time
 %     window
-%     samplingFrequency - is the signal acquisition sampling frequency.
+%     fs - is the signal acquisition sampling frequency.
 % 
-% OUTPUTS: PSTH - is NxM array. N signals, M time points. tiggerSummary - is
-% a 1xM array with M time points. sweeps - is an integer containing the trial
-% number. timeaxis - is a 1xM array.
+% OUTPUTS: 
+%     PSTH - is NxM array. N signals, M time points. 
+%     tiggerSummary - is a 1xM array with M time points. 
+%     sweeps - is an integer containing the trial number. 
+%     timeaxis - is a 1xM array.
 % 
-% The kicking out process needs to be implemented in a dynamical form. The
-% events after the (first) spike channel are a guide to kick row of the
-% spikes out. For now, we will kick out all of the rows which contain a true
-% in the observed time.
+%   Emilio Isaias-Camacho @ GrohLab 2019
 
 if binSz > 1
     disp('Assuming bin size given in milliseconds.')
