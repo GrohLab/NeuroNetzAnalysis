7b98c038c314a0112870ef07eea2891ab1c5e064
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2019-10-18 18:33:21 +0200

    Sensible naming of the function

---
+++ loadTriggerData.m
@@ -0,0 +1,46 @@
+function iOk = loadTriggerData(dataDir)
+iOk = false;
+binFiles = dir(fullfile(dataDir,'*.bin'));
+[~,expName,~] = fileparts( binFiles.name);
+% Loading the sampling frequency, the sorted clusters, and the conditions
+% and triggers.
+expSubfix = fullfile(dataDir,expName);
+try
+    load([expSubfix,'_sampling_frequency.mat'],'fs')
+catch
+    fprintf(1,'Seems like the kilosort-phy pipeline hasn''t been touched!\n')
+    return
+end
+assignin('base','fs',fs)
+try
+    load([expSubfix,'analysis.mat'],'Conditions','Triggers')
+catch
+    if exist([expSubfix, '_CondSig.mat'],'file')
+        getDelayProtocol(dataDir);
+    else
+        try
+            getConditionSignalsBF(fopen([expSubfix,'.smrx']))
+            getDelayProtocol(dataDir);
+        catch
+            fprintf(1,'Confusing naming. Cannot continue\n')
+            return
+        end
+    end
+    load([expSubfix,'analysis.mat'],'Conditions','Triggers')
+end
+assignin('base','Conditions',Conditions)
+assignin('base','Triggers',Triggers)
+try
+    load([expSubfix,'_all_channels.mat'],'sortedData')
+catch
+    try
+        importPhyFiles(dataDir);
+    catch
+        fprintf(1,'Error importing the phy files into Matlab format\n')
+        return
+    end
+    load([expSubfix,'_all_channels.mat'],'sortedData')
+end
+assignin('base','sortedData',sortedData)
+iOk = true;
+end
