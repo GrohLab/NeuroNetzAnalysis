01b01eec1d06b5d3ba2600c22798ab89d746fce1
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2019-10-25 14:44:10 +0200

    Majorly helpful function

---
+++ readClusterInfo.m
@@ -0,0 +1,15 @@
+function clInfo = readClusterInfo(fileName)
+clInfo = [];
+if ~exist(fileName, 'file')
+    fprintf(1, 'The given file doesn''t exist. Please verify it\n')
+    return
+end
+if ~copyfile(fileName,strrep(fileName, '.tsv', '.csv'))
+    fprintf(1,'There was an issue with the file.\n')
+    fprintf(1,'Please, verify your permissions in the folder.\n')
+    return
+end
+tempFileName = strrep(fileName, '.tsv', '.csv');
+clInfo = readtable(tempFileName);
+delete(tempFileName)
+end
