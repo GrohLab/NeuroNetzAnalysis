9cc48ba1c26961b36f0ba7270ca52af2e207eca7
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2019-11-07 14:10:50 +0100

    Input validation
    Cycle for different conditions with an specific test

--- statTests.m
+++ statTests.m
@@ -1,24 +1,34 @@
-function [H, P] = statTests(obs1, obs2, varargin)
-%STATTESTS performs the requested test between the two given observations
-%sets. The possible observations are the following: exact binomial,
-%McNemar, Chi^2, kstest.
-%
-%   [H, P] = statTests(obs1, obs2, testType)
-%
-%       INPUT
-%           - obs1 -- vector or matrix containing the first set of
-%           observations
-%           - obs2 -- vector or matrix containing the second set of
-%           observations
-%           - testType -- character vector describing the test to be
-%           performed ('binomial', 'mcnemar', 'chi2', 'kstest')
-%       OUTPUT
-%           - H -- logical vector or matrix indicating whether the compared
-%           observations reject the null hypothesis; if the P value is
-%           under 0.05
-%           - P -- vector or matrix containing the significance values.
+function [H, P] = statTests(dStack, condFlag, timeFlags, varargin)
+%STATTESTS function description
+% Emilio Isaias-Camacho @ GrohLab
+H = [];
+P = [];
+%% Validating the inputs
+p = inputParser;
+
+defaultTest = 'kstest';
+validTests = {'kstest','mcnemar','chi2','binomial'};
+checkTest = @(x) any(validatestring(x,validTests));
+
+checkStack = @(x) any([islogical(x), size(x) >= 1, numel(size(x)) == 3]);
+
+[~, Nt, NTa] = size(dStack);
+
+checkCondFlag = @(x) all([size(x,1) == NTa, islogical(x)]);
+checkTimeFlags = @(x) all([length(x) == Nt, islogical(x)]);
 
 
+addRequired(p, 'dStack', checkStack)
+addRequired(p, 'condFlag', checkCondFlag)
+addRequired(p, 'timeFlags', checkTimeFlags)
+addOptional(p, 'test', defaultTest, checkTest)
+
+p.KeepUnmatched = true;
+
+parse(p,dStack, condFlag, timeFlags, varargin{:})
+testType = p.Results.test;
+
+%% Preparatory variables
 
 end
 
