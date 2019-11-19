b60f421961a19a1edbda4792e9f4353e50f10c4a
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2019-11-19 19:04:46 +0100

    Logic draft implemented

--- statTests.m
+++ statTests.m
@@ -7,7 +7,7 @@ P = [];
 p = inputParser;
 
 defaultTest = 'kstest';
-validTests = {'kstest','mcnemar','chi2','binomial'};
+validTests = {'kstest','mcnemar','chi2','binomial','wilcoxon'};
 checkTest = @(x) any(validatestring(x,validTests));
 
 checkStack = @(x) any([islogical(x), size(x) >= 1, numel(size(x)) == 3]);
@@ -29,21 +29,49 @@ parse(p,dStack, condFlag, timeFlags, varargin{:})
 testType = p.Results.test;
 
 %% Preparatory variables
+
+
+% Number of alignment points per conditions
+Na = sum(condFlag, 1);
+
+% Test selector
+switch testType
+    case validTests{1} % KStest
+        statFun = @kstest2;
+    case validTests{2} % McNemar
+        fprintf(1,'Not yet implemented. Selecting kstest\n')
+        statFun = @kstest2;
+        
+    case validTests{3} % Chi^2
+        
+    case validTests{4} % Binomial
+        
+    case validTests{5} % Wilcoxon
+        
+end
+
+% Output variable
+
 % Number of conditions to cycle through
 Ncond = size(condFlag, 2);
 for cc1 = 1:Ncond
-    for cc2 = cc1:Ncond
-        if cc1 == cc2
-            % Comparing spontaneous versus evoked from the same condition
-            sponCount =...
-                squeeze(sum(dStack(2:end,timeFlags(1,:),condFlag(:,cc1)),2));
-            evokCount =...
-                squeeze(sum(dStack(2:end,timeFlags(2,:),condFlag(:,cc1)),2));
-            
-            continue
-        end
+    sponCountA =...
+        squeeze(sum(dStack(2:end,timeFlags(1,:),condFlag(:,cc1)),2));
+    evokCountA =...
+        squeeze(sum(dStack(2:end,timeFlags(2,:),condFlag(:,cc1)),2));
+    cc2 = cc1 + 1;
+    while cc2 <= Ncond
         % Comparing condition A versus condition B; spontaneous and evoked
+        sponCountB =...
+            squeeze(sum(dStack(2:end,timeFlags(1,:),condFlag(:,cc2)),2));
+        evokCountB =...
+            squeeze(sum(dStack(2:end,timeFlags(2,:),condFlag(:,cc2)),2));
+        
     end
+    % Same condition testing
+    [P, H] = signrank(sponCountA, evokCountA);
+    shufIdx = randperm()
+    [P, H] = signrank(SponCountA, evokCountA);
 end
 end
 
