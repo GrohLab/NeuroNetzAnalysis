ffc3afd58c1641ffc793c515f945f683a380dffb
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2020-04-29 18:56:12 +0200

    Create writeClusterInfo.m

---
+++ writeClusterInfo.m
@@ -0,0 +1,52 @@
+function iOk = writeClusterInfo(clInfo, fileName)
+iOk = false;
+% If the file exists, ask if it should be overwritten.
+if exist(fileName,'file')
+    owAns = questdlg(sprintf('%s exists. Overwirte?',fileName),...
+        'Overwrite','Yes','No','No');
+    if strcmpi(owAns,'no')
+        fprintf(1, 'No file writen.\n');
+        return
+    end
+end
+% Table given
+if ~istable(clInfo)
+    fprintf(1,'The given cluster table is not a MATLAB table. ')
+    fprintf(1,'Cannot continue.\n')
+    return
+end
+
+% Validation of the input file
+[dataDir, ~, ~] = fileparts(fileName);
+if isempty(dataDir)
+    dataDir = pwd;
+    fprintf(1, 'Warning: writing file in %s', dataDir);
+end
+
+fID = fopen(fileName, 'w');
+% Write header
+varNames = string(clInfo.Properties.VariableNames);
+for cvn = 1:length(varNames)-1
+    fprintf(fID, '%s\t', varNames(cvn));
+end
+fprintf(fID, '%s\n', varNames(cvn+1));
+% Write data
+[Ncl, Nv] = size(clInfo);
+for ccl = 1:Ncl
+    sep = '\t';
+    for cv = 1:Nv
+        if cv == Nv
+            sep = '\n';
+        end
+        switch varNames(cv)
+            case {"id","KSLabel","group","NeuronType"}
+                fprintf(fID,['%s',sep],clInfo{ccl,cv}{1});
+            case "firing_rate"
+                fprintf(fID,['%f spk/s',sep],clInfo{ccl,cv});
+            otherwise
+                fprintf(fID,['%f',sep],clInfo{ccl,cv});
+        end
+    end
+end
+fclose(fID);
+iOk = true;
