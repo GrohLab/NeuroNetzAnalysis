0e37134bdf94e71df8e0e078826049d3c99e59f7
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2020-10-08 11:01:04 +0200

    Added few measurements to the power spectrum

--- getSpectralEntropy.m
+++ getSpectralEntropy.m
@@ -1,4 +1,4 @@
-function [evals] = getSpectralEntropy(signal, dim)
+function [evals, mdl, gof] = getSpectralEntropy(signal, dim, fs)
 %GETSPECTRALENTROPY calculates the entropy for the given spectrum
 %   Detailed explanation goes here
 if ~exist('dim','var') || isempty(dim)
@@ -8,20 +8,26 @@ mg2db = @(x) 20*log10(abs(x));
 [rs, nc] = size(signal);
 Nwf = [rs, nc]*([dim-1;dim]==1);
 Ns = [rs, nc]*([dim;dim-1]==1);
-wx = log10(ceil(Ns/2):Ns)';
+wx = log10((ceil(Ns/2)+(1-mod(Ns,2))):Ns)';
 posIdx = [false(floor(Ns/2),1);true(ceil(Ns/2),1)];
 evals = zeros(Nwf, 1);
-ft = mg2db(fftshift(fft(signal,[],dim),dim));
+ft = fftshift(fft(signal,[],dim),dim); dw = fs/size(fs,1);
+mt = abs(ft);
+pt = angle(ft);
+dbft = mg2db(ft);
 gof = evals;
 mdl = zeros(2, Nwf);
 for cs = 1:Nwf
     if dim == 1
-        p = ft(:,cs);
+        p = mt(:,cs);
+        dp = dbft(:, cs);
     else
-        p = ft(cs,:);
+        p = mt(cs,:);
+        dp = dbft(cs, :);
     end
-    evals(cs) = getEntropyFromPDF(p);
-    mdl(:,cs) = fit_poly(wx, p(posIdx), 1);
-    gof(cs) = goodnessFit([wx, p(posIdx)], mdl(:,cs));
+    evals(cs) = abs(getEntropyFromPDF(dp(posIdx)));
+    mdl(:,cs) = fit_poly(wx, dp(posIdx), 1);
+    gof(cs) = goodnessFit([wx, dp(posIdx)], mdl(:,cs));
+    wcp = getWaveformCriticalPoints(dp(posIdx), 1/dw);
 end
 end
