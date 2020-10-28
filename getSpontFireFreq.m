92013806a435aa4e1d3977ccf971a4f93b4d66e9
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2020-10-28 09:57:35 +0100

    Added output of the considered spikes and the time difference sum

--- getSpontFireFreq.m
+++ getSpontFireFreq.m
@@ -1,9 +1,10 @@
-function [firingRatePerCluster, deltaTrigTime] =...
+function [firingRatePerCluster, deltaTrigTimeSum, sponSpks] =...
     getSpontFireFreq(spks, trigs, consTime, fs, bufTime)
 %GETSPONTFIREFREQ computes the firing rate outside of the specified
 %triggers leaving a time buffer around the pulse. 
 %
-%   [firingRatePerCluster] = getSpontFireFreq(spks, trigs, fs, <bufCent>)
+%   [firingRatePerCluster, deltaTrigTime] = getSpontFireFreq(spks, trigs,
+%                                                           fs, bufTime)
 %
 %   INPUTS:
 %       spks - spike times or subscripts for each cluster in a cell array
@@ -17,15 +18,24 @@ function [firingRatePerCluster, deltaTrigTime] =...
 %   OUTPUT:
 %       firingRatePerCluster - a column vector containing the firing rate
 %                              per cluster in the given order.
+%       deltaTrigTimeSum - a column vector containing the time considered
+%                          as spontaneous
+%       sponSpks - a cell array with the spikes per cluster within the
+%                  considered time in consTime and outside the given
+%                  triggers in trigs.
+% Emilio Isa?as-Camacho @GrohLab 2020
 
 spksFlag =...
     cellfun(@(x) round(x./fs) > consTime(1) & round(x./fs) <= consTime(2),...
     spks, 'UniformOutput', 0);
 consSpks = cellfun(@(x,y) x(y), spks, spksFlag, 'UniformOutput', 0);
-compareTimes =@(x) arrayfun(@(z) any(trigs(:,1) < z & trigs(:,2)+(bufTime*fs) >= z), x);
+compareTimes =@(x) arrayfun(@(z) any(trigs(:,1) < z &...
+    trigs(:,2)+(bufTime*fs) >= z), x);
 evokSpksFlags = cellfun(@(x) compareTimes(x), consSpks, 'UniformOutput', 0);
 NSpkSpont = cellfun(@(x) sum(~x), evokSpksFlags);
 trigs = trigs(trigs(:,2) <= round(consTime(2) * fs),:);
-deltaTrigTime = sum([trigs(:,1); round(consTime(2) * fs)] - [0; trigs(:,2)])/fs;
-firingRatePerCluster = NSpkSpont./deltaTrigTime;
+deltaTrigTimeSum = sum([trigs(:,1); round(consTime(2) * fs)] -...
+    [0; trigs(:,2)])/fs;
+firingRatePerCluster = NSpkSpont./deltaTrigTimeSum;
+sponSpks = cellfun(@(x,y) x(~y), consSpks, evokSpksFlags, 'UniformOutput', 0);
 end
