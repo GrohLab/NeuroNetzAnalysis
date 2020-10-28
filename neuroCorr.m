d1c635f429a670b791c82da93ed45a20233d00f3
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2020-10-28 16:45:12 +0100

    Finalized the correction for the auto correlograms

--- neuroCorr.m
+++ neuroCorr.m
@@ -3,10 +3,11 @@ function [corrStackCells] = neuroCorr(spks, tmReach, binSz, fs)
 %   Detailed explanation goes here
 % getStacks(spT,alignP,ONOFF,timeSpan,fs,fsLFP,consEvents,...)
 corrStackCells = cell(length(spks),1); Ncl = length(spks);
+zeroCorr = ceil(tmReach * fs) + 1;
 for ccl = 1:size(spks, 1)
     corrStackCells(ccl) = {squeeze(sum(getStacks(false, spks{ccl}, 'on',...
         [-tmReach, tmReach], fs, fs, spks(ccl+1:Ncl)),3))};
-    corrStackCells{ccl}(2,:) = [];
+    corrStackCells{ccl}(2,:) = []; corrStackCells{ccl}(:,zeroCorr) = 0;
 end
 end
 % corrStackCells = cellfun(@(x) squeeze(sum(getStacks(false, x, 'on',...
