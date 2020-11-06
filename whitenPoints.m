d5611e0eb05955a6cdac5e33f6c25ac2ffa9995f
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2020-11-06 19:40:14 +0100

    Return the mean of the points

--- whitenPoints.m
+++ whitenPoints.m
@@ -1,9 +1,10 @@
-function [ptsW, Wd] = whitenPoints(pts)
+function [ptsW, Wd, ptsMu] = whitenPoints(pts)
 %WHITENPOINTS computes the whitening matrix of and transforms the given
 %points without their mean.
 
 % Remove the mean
-pts = pts - mean(pts);
+ptsMu = mean(pts);
+pts = pts - ptsMu;
 % Compute the covariance matrix
 sig = cov(pts);
 % Compute the whitening matrix
