de3c9cd5c1a365bfcbc6e938e7af581cf2d127f4
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2021-08-02 14:26:08 +0200

    Improved reading of the RollerPosition file

--- readRollerPositionsFile.m
+++ readRollerPositionsFile.m
@@ -3,13 +3,13 @@ function rollerposition = readRollerPositionsFile(filepath)
 %   Detailed explanation goes here
 
 fID = fopen(filepath, 'r');
-rollerline = textscan(fID, '%d,2021-%d-%dT%d:%d:%f+02:00');
+rollerline = textscan(fID, '%d,2021-%*d-%*dT%d:%d:%f+02:00');
+% Different types of files.
+% % % To implement 
 fclose(fID);
-rollerx = int16(rollerline{1}); rollert = string(rollerline{4})+":"+...
-    rollerline{5}+":"+rollerline{6}; rollert = duration(rollert);
+rollerx = int16(rollerline{1}); rollert = string(rollerline{2})+":"+...
+    rollerline{3}+":"+rollerline{4}; rollert = duration(rollert);
 rollert.Format = 'hh:mm:ss.SSSSSSS';
 rollerposition = table(rollerx, rollert, ...
     'VariableNames', {'RollerX','RollerT'});
-
 end
-
