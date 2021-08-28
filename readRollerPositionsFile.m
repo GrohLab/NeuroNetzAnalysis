219f04830715439bbbb2146a09ed521b495c2518
Emilio Isaías-Camacho <isaias-camacho@physiologie.uni-heidelberg.de>
2021-08-28 13:56:06 +0200

    Included the newer file format
    Using Arduino's timestamps instead of Bonsai's

--- readRollerPositionsFile.m
+++ readRollerPositionsFile.m
@@ -4,12 +4,18 @@ function rollerposition = readRollerPositionsFile(filepath)
 
 fID = fopen(filepath, 'r');
 rollerline = textscan(fID, '%d,2021-%*d-%*dT%d:%d:%f+02:00');
-% Different types of files.
-% % % To implement 
 fclose(fID);
-rollerx = int16(rollerline{1}); rollert = string(rollerline{2})+":"+...
-    rollerline{3}+":"+rollerline{4}; rollert = duration(rollert);
-rollert.Format = 'hh:mm:ss.SSSSSSS';
-rollerposition = table(rollerx, rollert, ...
-    'VariableNames', {'RollerX','RollerT'});
+if all(cellfun(@(x) ~isempty(x), rollerline))
+    % Roller file corresponding to the Bonsai timestamps
+    rollerx = int16(rollerline{1}); rollert = string(rollerline{2})+":"+...
+        rollerline{3}+":"+rollerline{4}; rollert = duration(rollert);
+    rollert.Format = 'hh:mm:ss.SSSSSSS';
+    rollerposition = table(rollerx, rollert, ...
+        'VariableNames', {'RollerX','RollerT'});
+else
+    % Roller file corresponding to Arduino's micros() function.
+    rollerposition = importdata(filepath);
+    rollerposition(:,1) = unwrap(rollerposition(:,1), 2^15);
+    rollerposition(:,2) = unwrap(rollerposition(:,2), 2^31);
+end
 end
