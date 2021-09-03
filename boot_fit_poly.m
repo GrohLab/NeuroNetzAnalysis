5723f1a765a5f326b4a0862fb221966de1fd8ac8
Emilio Isaías-Camacho <isaias-camacho@physiologie.uni-heidelberg.de>
2021-09-03 11:08:28 +0200

    Included a threshold for considering inlines

--- boot_fit_poly.m
+++ boot_fit_poly.m
@@ -1,4 +1,4 @@
-function [wmdl, inln] = boot_fit_poly(pts, n, ptsPer, it)
+function [wmdl, inln] = boot_fit_poly(pts, n, ptsPer, it, th)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 Npts = size(pts,1);
@@ -10,18 +10,18 @@ inlnit = false(size(pts,1), it);
 for cit = 1:it
     selSubs = randperm(Npts, cpts);
     cmdl(:, cit) = fit_poly(pts(selSubs,1), pts(selSubs,2), n);
-    [inlnit(:,cit), mserr(cit)] = meansqrerr(pts, cmdl(:, cit), n);
+    [inlnit(:,cit), mserr(cit)] = meansqrerr(pts, cmdl(:, cit), n, th);
 end
 consensus = mean(inlnit,2) > 0.99;
 wmdl = fit_poly(pts(consensus, 1), pts(consensus, 2), n);
 % mw = 1./(mserr.*sum(1./mserr)); wmdl = cmdl*mw;
-inln = meansqrerr(pts, wmdl, n);
+inln = meansqrerr(pts, wmdl, n, th);
 end
 
-function [inln, mserr] = meansqrerr(pts, mdl, n)
+function [inln, mserr] = meansqrerr(pts, mdl, n, th)
 err = pts(:,2) - ((pts(:,1).^(n:-1:0)) * mdl);
 % Starting with z-score in between normal 1 std ~0.63. Later on could be a
 % user defined threshold.
-inln = abs(zscore(err)) <= 0.63;
+inln = abs(zscore(err)) <= th;
 mserr = (err'*err)/size(pts,1);
 end
