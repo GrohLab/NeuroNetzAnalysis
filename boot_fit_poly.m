1413495ccd883e6a42a303e25ac07c1416db3119
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2021-09-03 17:39:00 +0200

    Removed old commented code

--- boot_fit_poly.m
+++ boot_fit_poly.m
@@ -4,8 +4,6 @@ function [wmdl, inln] = boot_fit_poly(pts, n, ptsPer, it, th)
 Npts = size(pts,1);
 cpts = round(Npts * ptsPer);
 mserr = zeros(it,1); cmdl = zeros(n+1, it);
-% pmdl = fit_poly(pts(:,1), pts(:,2), n);
-% [inln, mserr_pop] = meansqrerror(pts, pmdl, n);
 inlnit = false(size(pts,1), it);
 for cit = 1:it
     selSubs = randperm(Npts, cpts);
@@ -14,14 +12,12 @@ for cit = 1:it
 end
 consensus = mean(inlnit,2) > 0.99;
 wmdl = fit_poly(pts(consensus, 1), pts(consensus, 2), n);
-% mw = 1./(mserr.*sum(1./mserr)); wmdl = cmdl*mw;
 inln = meansqrerr(pts, wmdl, n, th);
 end
 
 function [inln, mserr] = meansqrerr(pts, mdl, n, th)
 err = pts(:,2) - ((pts(:,1).^(n:-1:0)) * mdl);
-% Starting with z-score in between normal 1 std ~0.63. Later on could be a
-% user defined threshold.
+% Starting with z-score in between normal 1 std ~0.63. 
 inln = abs(zscore(err)) <= th;
 mserr = (err'*err)/size(pts,1);
 end
