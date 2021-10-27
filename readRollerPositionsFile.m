fd0d3560a8238da1a83eceb2415c2b7fb6f19c14
Emilio Isaías-Camacho <isaias-camacho@physiologie.uni-heidelberg.de>
2021-10-27 18:14:59 +0200

    Included the trigger time from Arduino

--- readRollerPositionsFile.m
+++ readRollerPositionsFile.m
@@ -1,7 +1,7 @@
-function rollerposition = readRollerPositionsFile(filepath)
+function [rollerposition, tTimes] = readRollerPositionsFile(filepath)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-
+tTimes = [];
 fID = fopen(filepath, 'r');
 rollerline = textscan(fID, '%d,2021-%*d-%*dT%d:%d:%f+02:00');
 fclose(fID);
@@ -13,9 +13,30 @@ if all(cellfun(@(x) ~isempty(x), rollerline))
     rollerposition = table(rollerx, rollert, ...
         'VariableNames', {'RollerX','RollerT'});
 else
+    fnOpts = {"UniformOutput", false};
+    ops = delimitedTextImportOptions("NumVariables", 2);
+    ops.DataLines = [1, Inf];
+    ops.Delimiter = ",";
+    ops.VariableNames = ["RoT", "Time_mus"];
+    ops.VariableTypes = ["string", "double"];
+    ops.ExtraColumnsRule = "ignore";
+    ops.EmptyLineRule = "read";
     % Roller file corresponding to Arduino's micros() function.
-    rollerposition = importdata(filepath);
-    rollerposition(:,1) = unwrap(rollerposition(:,1), 2^15);
-    rollerposition(:,2) = unwrap(rollerposition(:,2), 2^31);
+    rollTrigTimes = readtable(filepath, ops);
+    rollTrigTimes.Time_mus = unwrap(rollTrigTimes.Time_mus, 2^32);
+    % Is there any 
+    trigFlag = isnan(str2double(rollTrigTimes.RoT));
+    trigLetter = unique(rollTrigTimes.RoT(trigFlag)); 
+    if ~isempty(trigLetter)
+        trigNum = size(trigLetter, 1);
+        fprintf(1, "Found %d trigger(s): %s\n", trigNum,...
+            sprintf("%s ",trigLetter))
+        % Comparing the content of the table with all found letters
+        % corresponding to specific triggers
+        tFlags = arrayfun(@(x) ismember(rollTrigTimes.RoT(trigFlag), x),...
+            trigLetter', fnOpts{:}); 
+        % Reading those times which correspond only to the specific trigger
+        tTimes = cellfun(@(x) rollTrigTimes.Time_mus(x), tFlags, fnOpts{:});
+    end
 end
 end
