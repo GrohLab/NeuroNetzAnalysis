54c0024ceb71953327a457cce397046ba2a483ab
Emilio Isaías-Camacho <isaias-camacho@physiologie.uni-heidelberg.de>
2021-12-30 23:39:29 +0100

    Dealing with several bin file cases

--- prepare64ChanBin4KS.m
+++ prepare64ChanBin4KS.m
@@ -16,11 +16,11 @@ defChan = 64;
 checkChan = @(x) all([isPositiveIntegerValuedNumeric(x), numel(x) == 1]);
 
 p.addRequired('dataDir', checkDataDir)
-p.addOptional('BinFileName', defOutBinName, checkOutBinName);
-p.addOptional('fs', defFs, checkFs);
-p.addOptional('ChanNumber', defChan, checkChan);
+p.addParameter('BinFileName', defOutBinName, checkOutBinName);
+p.addParameter('fs', defFs, checkFs);
+p.addParameter('ChanNumber', defChan, checkChan);
 
-p.parse(dataDir, varargin{:});
+parse(p, dataDir, varargin{:});
 
 dataDir = p.Results.dataDir;
 outBinName = p.Results.BinFileName;
@@ -28,17 +28,23 @@ fs = p.Results.fs;
 Nch = p.Results.ChanNumber;
 
 %% Validation of inputs
-iOk = false;
-
-recFile = dir(fullfile(dataDir, 'Recording*.bin'));
-if isempty(recFile)
-    fprintf(2, 'No recording file found!\n')
-    fprintf(2, 'Make sure you provide a directory with a recording file');
-    fprintf(2, ' in it!\n');
+iOk = false; getName = @(x) string(x.name);
+% Search for Recording*.bin files in the given folder
+recFiles = dir(fullfile(dataDir, 'Recording*.bin'));
+if isempty(recFiles)
+    fprintf(1, 'No recording file found!\n')
+    fprintf(1, 'Make sure you provide a directory with a recording file');
+    fprintf(1, ' in it!\n');
     return
 end
+
 % Validation for existing output file
-outFullName = fullfile(dataDir, outBinName);
+outBinName = string(outBinName);
+if ~contains(outBinName, ".bin")
+    outFullName = fullfile(dataDir, outBinName + ".bin");
+else
+    outFullName = fullfile(dataDir, outBinName);
+end
 if exist(outFullName,'file')
     ovwtAns = questdlg(...
         sprintf('Warning! File %s exists. Overwrite?',outFullName),...
@@ -48,7 +54,117 @@ if exist(outFullName,'file')
         return
     end
 end
+%% Processing files
+% How many files are in the folder
+Nrf = numel(recFiles);
+% If there are more than 1 files in the folder, ask the used which are
+% going to be merged.
+if Nrf > 1
+    selFile = listdlg("ListString", arrayfun(getName, recFiles));
+    % Of course, if the user presses cancel, the function ends without
+    % creating any file.
+    if isempty(selFile)
+        fprintf(1, "Canceled. No file created!\n")
+        return
+    end
+    % Getting only those desired files.
+    recFiles = recFiles(selFile); Nrf = numel(recFiles);
+    fNames = arrayfun(getName, recFiles);
+    if Nrf > 1
+        % Merging in a given order
+        fileOrder = (1:Nrf)';
+        defInput = num2cell(num2str(fileOrder));
+        answr = inputdlg(fNames,'File order',[1, 60], defInput);
+        nFileOrder = str2double(answr);
+        if ~isempty(answr) && sum(abs(fileOrder - nFileOrder)) ~= 0
+            fprintf(1,'File order changed...\n')
+            recFiles = recFiles(nFileOrder);
+        end
+    end
+end
+fNames = arrayfun(getName, recFiles);
+try
+    mem = memory;
+catch
+   fprintf(1, "Memory pack unavailable!\n")
+end
+% Overwrite the output file.
+fPerm = 'w';
+for cf = 1:Nrf
+    fprintf(1, "File: %s\n", fNames(cf));
+    % How many bytes is this file?
+    fWeight = recFiles.bytes;
+    % How many bytes are available in this computer. Occupying 85%
+    mxBytes = 0.8 * mem.MaxPossibleArrayBytes;
+    % Number of samples in the file with Nch channels. 2 bytes from uint16. 
+    Ns = fWeight/(2*Nch); Nms = mxBytes/(4*Nch);
+    if Ns > Nms
+        Ns = Nms;
+    else
+        Ns = Inf;
+    end
+    dfID = openDataFile(fullfile(dataDir, fNames(cf)));
+    chnk = 1;
+    while ~feof(dfID)
+        fprintf(1, "Reading batch %d...\n", chnk)
+        chnk = chnk + 1;
+        dataBuff = readDataFileAndMedianFilter(dfID, Ns, Nch);
+        ofID = createOutputFile(outFullName, fPerm);
+        fprintf(1, "Writing...\n")
+        fwrite(ofID, dataBuff, "int16")
+        fPerm = 'a'; fclose(ofID);
+    end
+    fclose(dfID);
+end
+iOk = true;
+%% Writing useful files
+ffoID = fopen(fullfile(dataDir,outBinName + "_fileOrder.txt"),'w');
+fprintf(ffoID,"%s\n",arrayfun(getName, recFiles));
+fprintf(ffoID, "%s.bin", outBinName); fclose(ffoID);
+save(fullfile(dataDir, outBinName + "_sampling_frequency.mat"), 'fs')
+end
 
+function fID = openDataFile(fPath)
+fID = fopen(fPath, 'r');
+if fID < 3
+    % Error by opening data file
+    [~, fName] = fileparts(fPath);
+    fprintf(1, 'Unable to open %s!\n', fName)
+    fprintf(1, 'Verify that no other process is running with it and')
+    fprintf(1, ' try again.\n');
+    return
+end
+end
+
+function [dataBuff, buffMed] = readDataFileAndMedianFilter(fID, Ne, Nch)
+% As a result to removing the median of the channels, the data will take
+% negative values. This is why I need to convert the data to signed 32-bit
+% integers before substracting the median. Once the data is median  free,
+% then it can be converted back to signed 16-bit integer.
+dataBuff = fread(fID, [Nch, Ne], 'uint16=>int32');
+buffMed = median(dataBuff);
+dataBuff = int16(dataBuff - buffMed);
+end
+
+function fID = createOutputFile(fPath,fPerm)
+fID = fopen(fPath, fPerm);
+if fID < 3
+    fprintf(1, 'Unable to create/open output file!\n');
+    fprintf(1, 'Please verify the file and directory!\n');
+    return
+end % Data file identifier validation
+end
+
+function fID = createMedianFile(fPath) %#ok<DEFNU>
+fID = fopen(fPath, 'w');
+if fID < 3
+    fprintf(1, 'Unable to create median file!\n');
+    fprintf(1, 'Please verify the file and directory!\n');
+    return
+end % Median File identifier validation
+end
+
+%{
 if numel(recFile) < 2
     % One recording file in directory
     dfID = openDataFile(fullfile(dataDir, recFile.name));
@@ -81,41 +197,4 @@ else
     end
 end % More than one Recording* file in the directory
 
-end
-
-function fID = openDataFile(fPath)
-fID = fopen(fPath, 'r');
-if fID < 3
-    % Error by opening data file
-    [~, fName] = fileparts(fPath);
-    fprintf(1, 'Unable to open %s!\n', fName)
-    fprintf(1, 'Verify that no other process is running with it and')
-    fprintf(1, ' try again.\n');
-    return
-end
-end
-
-function [dataBuff, buffMed] = readDataFileAndMedianFilter(fID, Ne)
-dataBuff = fread(fID, Ne, 'uint16=>int32');
-dataBuff = reshape(dataBuff, 64, []);
-buffMed = int16(median(dataBuff));
-dataBuff = int16(dataBuff - buffMed);
-end
-
-function fID = createOutputFile(fPath)
-fID = fopen(fPath, 'w');
-if fID < 3
-    fprintf(1, 'Unable to create output file!\n');
-    fprintf(1, 'Please verify the file and directory!\n');
-    return
-end % Data file identifier validation
-end
-
-function fID = createMedianFile(fPath)
-fID = fopen(fPath, 'w');
-if fID < 3
-    fprintf(1, 'Unable to create median file!\n');
-    fprintf(1, 'Please verify the file and directory!\n');
-    return
-end % Median File identifier validation
-end
+%}
