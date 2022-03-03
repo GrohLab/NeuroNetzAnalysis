628524925efc62073369337f1700b167afe03412
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2022-03-03 17:26:25 +0100

    No change

--- zscoreSignificance.m
+++ zscoreSignificance.m
@@ -5,7 +5,7 @@ fnOpts = {'UniformOutput', false}; mymean = @(x) mean(x,2,"omitnan");
 thcmp = @(x,y) x > reshape(y,1,1,[]);
 nDist = makedist('Normal', "mu", 0, "sigma", 1);
 % Replace for user input thresholds
-alph = [0.95, 0.98, 0.99, 0.995, 0.998, 0.999]; 
+alph = [0.95, 0.98, 0.99, 0.995, 0.998, 0.999];
 signTh = arrayfun(@(y) fminbnd(@(x) norm(nDist.cdf(x) - y, 2), 1, 5),...
     alph);
 [~, muZ, sigZ] = cellfun(@(x) zscore(x, 1, 2), Counts(:,1), fnOpts{:});
