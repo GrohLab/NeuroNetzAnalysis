236f74931eca96d3205d09b3c9b3d3dd4e571b0c
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2022-03-14 18:20:10 +0100

    Density with zero values for empty spike trains

--- getBasicSts.m
+++ getBasicSts.m
@@ -9,14 +9,24 @@ end
 foSts = @(x) [median(x), mean(x), std(x)];
 expnd = @(x, r) linspace(x(1), x(end), 1+ceil(diff(x([1,end]))/res));
 fnOpts = {'UniformOutput', false};
+erOpts = {'ErrorHandler', @emptySpkTrain};
 ksOpts = {'Function','pdf'};
 %% 
-spkDom = expnd(cStruct.Response_window_s, res);
+
+spkDom = expnd(cStruct.Response_window_s, res); N = length(spkDom);
+% Median, Mean, and STD
 sFOS = cellfun(@(x) cellfun(foSts, x, fnOpts{:}), spkCell, fnOpts{:});
 sFOS = cellfun(@(x) cat(1, x{:}), sFOS, fnOpts{:});
+% Empty spike trains
+eptyFlag = cellfun(@(x) cellfun(@(y) isempty(y), x), spkCell, fnOpts{:});
 % Inverse cumulative density function of the spikes
-sH = cellfun(@(x) cellfun(@(y) ksdensity(y, spkDom, ksOpts{:}), ...
-    x, fnOpts{:}, "ErrorHandler", @emptySpkTrain), spkCell, fnOpts{:});
+sH = cellfun(@(x) cellfun(@(y) ksdensity(y, spkDom, ksOpts{:}), x, ...
+    fnOpts{:}, erOpts{:}), spkCell, fnOpts{:});
+for ca = 1:length(sH)
+    for cb = reshape(find(eptyFlag{ca}),1,[])
+        sH{ca}{cb} = repmat(sH{ca}{cb}, 1, N);
+    end
+end
 % Mahalanobis distance in matrix form
 sMh = cellfun(@(x) cellfun(@(y) squareform(pdist(y(:), "mahalanobis")), ...
     x, fnOpts{:}), spkCell, fnOpts{:});
