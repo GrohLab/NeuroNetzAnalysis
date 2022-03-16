fc4a5438111b90d0e585ad547244134f8c0517f5
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2022-03-16 13:44:14 +0100

    Included configuration structure
    Substituting several optional arguments to the function with a structure input

--- getResponseProbability.m
+++ getResponseProbability.m
@@ -1,15 +1,14 @@
-function [p_f, pEvok, pSpon] = getResponseProbability(relSpkTms,respWin,varargin)
+function [p_f, pEvok, pSpon] = getResponseProbability(relSpkTms,cStruct,varargin)
 %GETRESPONSEPROBABILITY The trial is classified as responsive (true) when
 %the spiking activity within the response window exceeds the spiking
 %activity in the spontaneous window
-%   [p_f] = getResponseProbability(relSpkTms,respWin)
-%   [p_f] = getResponseProbability(relSpkTms,respWin,varargin)
+%   [p_f] = getResponseProbability(relSpkTms,cStruct)
+%   [p_f] = getResponseProbability(relSpkTms,cStruct,varargin)
 %   INPUTS:
 %       relSpkTms - 1xC structure array with 'name' and 'SpikeTimes' as
 %                   fields. Produced in DE_Jittering and poolExperiments
 %                   scripts.
-%       respWin - 1x2 array representing the time when the neuron is
-%                 expected to respond.
+%       cStruct - structure containing the information of the 
 %   NAME-VALUE:
 %       'sponWin' - 1x2 array representing the time when the neuron is
 %                   expected to respond. Default is -flip(respWin)
@@ -22,7 +21,7 @@ function [p_f, pEvok, pSpon] = getResponseProbability(relSpkTms,respWin,varargin
 %   OUTPUT:
 %       p_f - NxC array with the probability of response for each neuron N
 %             and condition C. 
-%       pSpon - NxC arraz with the probability of spiking occurrances
+%       pSpon - NxC array with the probability of spiking occurrances
 %               within the spontaneous window
 %       pEvok - NxC array with the probability of spiking occurrances
 %               within the response window
@@ -32,17 +31,7 @@ function [p_f, pEvok, pSpon] = getResponseProbability(relSpkTms,respWin,varargin
 
 p = inputParser;
 % Required arguments
-checkSpkStruct = @(x) any([isstruct(x), isfield(x,{'name','SpikeTimes'})]);
-
-checkIntWin = @(x) any([isnumeric(x), numel(x) == 2, diff(x) > 0]);
-% Required parameters
-%   Spontaneous window
-defSponWin = -flip(respWin);
-%   Time limits for time histogram
-defTmWin = [-0.1, 0.1];
-%   Bin width in seconds
-defBinWdth = 1e-3;
-checkBinWdth = @(x) all([isnumeric(x), x <= diff(respWin), numel(x)==1]);
+checkSpkStruct = @(x) all([isstruct(x), isfield(x,{'name','SpikeTimes'})]);
 
 % Optional parameter
 %   Verbose
@@ -51,24 +40,20 @@ checkVerbose = @(x) all([numel(x) == 1, isnumeric(x) | islogical(x)]);
 
 % Parsing
 p.addRequired('relSpkTms', checkSpkStruct);
-p.addRequired('respWin', checkIntWin);
-p.addParameter('sponWin', defSponWin, checkIntWin);
-p.addParameter('timeLimits',defTmWin, checkIntWin);
-p.addParameter('binWidth',defBinWdth, checkBinWdth);
+p.addRequired('cStruct', @checkConfigStruct);
 p.addOptional('verbose', defVerbose, checkVerbose); 
-
 p.KeepUnmatched = true;
-
-p.parse(relSpkTms, respWin, varargin{:});
+p.parse(relSpkTms, cStruct, varargin{:});
 
 % Results
 relSpkTms = p.Results.relSpkTms;
-respWin = p.Results.respWin;
-sponWin = p.Results.sponWin;
-tmLm = p.Results.timeLimits;
-binWdth = p.Results.binWidth;
+cStruct = p.Results.cStruct;
 verbose = p.Results.verbose;
 %% Validation of time windows
+respWin = cStruct.Response_window_s;
+sponWin = -flip(respWin);
+tmLm = cStruct.Viewing_window_s;
+binWdth = cStruct.BinSize_s;
 respDelta = diff(respWin);
 sponDelta = diff(sponWin);
 tmDelta = diff(tmLm);
@@ -113,6 +98,5 @@ p_f = cellfun(@(y) cellfun(@(x)...
     mean(getSpikingActivityIn(x,respWin)...
     > getSpikingActivityIn(x,sponWin)), y), p_f, fnOpts{:});
 p_f = cat(2, p_f{:});
-
 end
 
