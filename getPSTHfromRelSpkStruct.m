4369e173d4f99c67e2068fd26436961a3fd7e47e
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2022-03-21 15:40:07 +0100

    Returned un-normalised PSTH and # trials

--- getPSTHfromRelSpkStruct.m
+++ getPSTHfromRelSpkStruct.m
@@ -1,4 +1,4 @@
-function [PSTH, psthTx] = getPSTHfromRelSpkStruct(relSpkStrct, confgStruct)
+function [PSTH, psthTx, Na] = getPSTHfromRelSpkStruct(relSpkStrct, confgStruct)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 catClSpks = @(x, c) cat(2, x.SpikeTimes{c,:});
@@ -13,7 +13,7 @@ mdl = fit_poly([1,Npsth], timeLapse, 1);
 psthTx = ((1:Npsth).^[1;0])'*mdl;
 
 PSTH = arrayfun(@(x) arrayfun(@(y) histcounts(catClSpks(relSpkStrct(x), y),...
-    htOpts{:})./Na(x), 1:Ncl(x), fnOpts{:}), 1:Ncond, fnOpts{:});
+    htOpts{:}), 1:Ncl(x), fnOpts{:}), 1:Ncond, fnOpts{:});
 PSTH = cellfun(@(x) cat(1, x{:}), PSTH, fnOpts{:});
 PSTH = cat(3, PSTH{:});
 end
