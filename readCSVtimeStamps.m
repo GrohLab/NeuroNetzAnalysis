1a3a84c943361680e0b6910c47da52c58c6766b2
Emilio Isaías-Camacho <isaias-camacho@physiologie.uni-heidelberg.de>
2022-05-30 17:25:44 +0200

    Including logical values in the conversion

--- readCSVtimeStamps.m
+++ readCSVtimeStamps.m
@@ -7,7 +7,8 @@ if exist(fPath, 'file')
     fID = fopen(fPath, 'r');lns = textscan(fID,'%s',Inf);[~] = fclose(fID);
     trigTime = datetime(clean4Date(lns{:}), dtOpts{:});
     trigTime = second(trigTime, "secondofday");
-    Val = str2double(clean4LV(lns{:}));
+    Val = arrayfun(@(x) str2num(x,"Evaluation","restricted"), ...
+        lower(clean4LV(string(lns{:})))); %#ok<ST2NM> 
     csvMat = [Val, trigTime];
 end
 end
