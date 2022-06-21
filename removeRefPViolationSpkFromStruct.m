fcaaf5f0bbf39cc25dcf1646bd3c0f269baab415
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2022-06-21 15:57:03 +0200

    Create removeRefPViolationSpkFromStruct.m

---
+++ removeRefPViolationSpkFromStruct.m
@@ -0,0 +1,5 @@
+function [cleanSpkStr] = removeRefPViolationSpkFromStruct(spkStruct, varargin)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+p = inputParser;
+end
