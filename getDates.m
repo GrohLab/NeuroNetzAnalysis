6f12d4813ad8d2887c7651473a59bbb6f0c8fa96
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2022-11-04 18:28:57 +0100

    Added an ending to the date extraction

--- getDates.m
+++ getDates.m
@@ -1,11 +1,14 @@
-function [fDates, dateFormStr] = getDates(fileNames, baseName)
+function [fDates, dateFormStr] = getDates(fileNames, baseName, endng)
 % Function to extract the date from the file name.
 % Extract machine readable 19-December-2021 15:32:12 from
 % Roller_position2021-12-19T15_32_12.csv, for example. "Roller_position"
 % would be the baseName, and an array with structures from dir function.
 dateFormStr = 'yyyy-MM-dd''T''HH_mm_ss';
 fnOpts = {"UniformOutput", false};
-getDate = @(x, y) extractBefore(extractAfter(x, y), ".");
+if ~exist("endng", "var")
+    endng = ".";
+end
+getDate = @(x, y) extractBefore(extractAfter(x, y), endng);
 switch class(fileNames)
     case 'struct'
         fDates = arrayfun(@(x) getDate(x.name, baseName), fileNames,...
