e431233c143d2733cdb1e00ef2dfd41eb88e1e97
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2023-03-07 17:54:28 +0100

    Trial information

--- NWB/getTrialInfo4NWB.m
+++ NWB/getTrialInfo4NWB.m
@@ -5,103 +5,143 @@ function [nwbObj, trial_timeseries] = ...
 
 %% Validate inputs
 p = inputParser();
-validateCondSel = @(x) isPositiveIntegerValuedNumeric(x) | (isstring(x) | ...
-    ischar(x));
+
+istxt = @(x) isstring(x) | ischar(x) | iscellstr(x);
+validateCondSel = @(x) isPositiveIntegerValuedNumeric(x) | istxt(x);
 
 addRequired(p, "nwbObj", @(x) isa(x, "NwbFile"))
 addRequired(p, "sessionPath", @(x) exist(x, 'dir'))
 addParameter(p, "ConditionSelect", "all", validateCondSel)
 
-parse(p, nwbObj, sessionPath)
+parse(p, nwbObj, sessionPath, varargin{:})
 
 nwbObj = p.Results.nwbObj;
 sessionPath = p.Results.sessionPath;
+condSel = p.Results.ConditionSelect;
 
 %% Auxiliary functions and variables
+fnOpts = {'UniformOutput', false};
 pathHere = @(x) fullfile(sessionPath, x);
 look4this = @(x) dir(pathHere(x));
 expandName = @(x) fullfile(x.folder, x.name);
 % Validating for files in the ephys directory
 cFiles = look4this("*analysis.mat");
-if isempty(cFiles)
-    fprintf(1, "Warning! No analysis file found in given directory!\n")
+fFiles = look4this("*_sampling_frequency.mat");
+pFiles = look4this("*_ParameterConfiguration.mat");
+if isempty(cFiles) || isempty(fFiles) || isempty(pFiles)
+    fprintf(1, "Warning! Necessary files missing in given directory!\n")
     fprintf(1, "Cannot continue!\n")
     return
 end
 cMF = matfile(expandName(cFiles));
-Conditions = cMF.Conditions; Triggers = cMF.Triggers;
-
-
-% Variable storing continuous signals from the trials.
-trial_timeseries = cell(size(data.trialIds)); 
-
-%%
-% NWB comes with default support for trial-based data.  These must be *TimeIntervals* that
-% are placed in the |intervals| property.  Note that |trials| is a special
-% keyword that is required for PyNWB compatibility.
-
-ephus = data.timeSeriesArrayHash.value{1};
-ephusUnit = data.timeUnitNames{data.timeUnitIds(ephus.timeUnit)};
-
-% lick direction and timestamps trace
-tsIdx = strcmp(ephus.idStr, 'lick_trace');
-bts = types.core.BehavioralTimeSeries();
-
-bts.timeseries.set('lick_trace_ts', ...
-    types.core.TimeSeries(...
-    'data', ephus.valueMatrix(:,tsIdx),...
-    'data_unit', ephusUnit,...
-    'description', ephus.idStrDetailed{tsIdx}, ...
-    'timestamps', ephus.time, ...
-    'timestamps_unit', ephusUnit));
-nwb.acquisition.set('lick_trace', bts);
-bts_ref = types.untyped.ObjectView('/acquisition/lick_trace/lick_trace_ts');
-
-% acousto-optic modulator input trace
-tsIdx = strcmp(ephus.idStr, 'aom_input_trace');
-ts = types.core.TimeSeries(...
-    'data', ephus.valueMatrix(:,tsIdx), ...
-    'data_unit', 'Volts', ...
-    'description', ephus.idStrDetailed{tsIdx}, ...
-    'timestamps', ephus.time, ...
-    'timestamps_unit', ephusUnit);
-nwb.stimulus_presentation.set('aom_input_trace', ts);
-ts_ref = types.untyped.ObjectView('/stimulus/presentation/aom_input_trace');
-
-% laser power
-tsIdx = strcmp(ephus.idStr, 'laser_power');
-ots = types.core.OptogeneticSeries(...
-    'data', ephus.valueMatrix(:,tsIdx), ...
-    'data_unit', 'mW', ...
-    'description', ephus.idStrDetailed{tsIdx}, ...
-    'timestamps', ephus.time, ...
-    'timestamps_unit', ephusUnit, ...
-    'site', types.untyped.SoftLink('/general/optogenetics/photostim'));
-nwb.stimulus_presentation.set('laser_power', ots);
-ots_ref = types.untyped.ObjectView('/stimulus/presentation/laser_power');
-
-% append trials timeseries references in order
-[ephus_trials, ~, trials_to_data] = unique(ephus.trial);
-for i=1:length(ephus_trials)
-    i_loc = i == trials_to_data;
-    t_start = find(i_loc, 1);
-    t_count = sum(i_loc);
-    trial = ephus_trials(i);
-    
-    trial_timeseries{trial}(end+(1:3), :) = {...
-        bts_ref int64(t_start) int64(t_count);...
-        ts_ref  int64(t_start) int64(t_count);...
-        ots_ref int64(t_start) int64(t_count)};
+Conditions = cMF.Conditions; Triggers = cMF.Triggers; clearvars("cMF");
+condNames = arrayfun(@(x) string(x.name), Conditions);
+if istxt(condSel)
+    if strcmp(condSel, "all")
+        % All conditions into the NWB file
+        allCond = contains(condNames, "all", "IgnoreCase", true);
+        condSel = setdiff(1:numel(Conditions),find(allCond));
+    else
+        % Selected conditions by their name or it might be a cell array
+        % with char arrays or strings
+        condSel = ismember(condNames, condSel);
+    end
 end
+
+%% Loading and preparing data
+% Triggers per selected condition
+trigPerCond = arrayfun(@(x) x.Triggers(:,1), Conditions(condSel), ...
+    fnOpts{:});
+% Number of trials per condition
+Na = cellfun(@(c) size(c,1), trigPerCond);
+% Trial types: Condition names
+consCondNames = arrayfun(@(c) string(c.name), Conditions(condSel));
+load(expandName(fFiles), "fs")
+load(expandName(pFiles), "configStructure")
+
+csFlag = arrayfun(@(x) ismember(x.ConsideredConditions, ...
+    consCondNames), configStructure, fnOpts{:});
+csFlag = find(cellfun(@any, csFlag),1,"first");
+configStructure = configStructure(csFlag); clearvars("csFlag");
+offst = configStructure.Viewing_window_s(1);
+tsNames = string(fieldnames(Triggers));
+% Trial duration
+trial_samples = ceil(diff(configStructure.Viewing_window_s)*fs);
+% Trial start in samples
+trial_start_sample = cellfun(@(c) c + ...
+    round(configStructure.Viewing_window_s(1)*fs), trigPerCond, fnOpts{:}); 
+ccfn = 1;
+N_signals = numel(tsNames); N_trial_types = numel(consCondNames);
+chopped_timeseries = cell(N_signals, N_trial_types);
+% Chopping signals around the triggers
+for cfn = tsNames(:)'
+    sgn = Triggers.(cfn);
+    chopped_timeseries_aux = cellfun(@(c) arrayfun(@(t) ...
+        sgn(t:t+trial_samples-1), c, fnOpts{:}), ...
+        trial_start_sample, fnOpts{:});
+    chopped_timeseries_aux = cellfun(@(st) cat(1, st{:}), ...
+        chopped_timeseries_aux, fnOpts{:});
+    chopped_timeseries(ccfn,:) = chopped_timeseries_aux;
+    ccfn = ccfn + 1;
+end
+% Freeing memory from auxiliary variables
+clearvars("sgn", "chopped_timeseries_aux")
+% Trial start in time
+trial_start_time_per_cond = cellfun(@(t) (t./fs) + offst, ...
+    trigPerCond, fnOpts{:});
+timestamps = cellfun(@(c) arrayfun(@(t) t:t+trial_samples-1, c, ...
+    fnOpts{:}), trial_start_sample, fnOpts{:});
+timestamps = cellfun(@(c) cat(2, c{:})./fs, timestamps, fnOpts{:});
+timestamps_all = cat(2, timestamps{:});
+trial_type = arrayfun(@(tt, tm) repmat(string(tt), tm, 1), ...
+    consCondNames, Na, fnOpts{:}); trial_type = cat(1, trial_type{:});
+%% Variable storing continuous signals from the trials.
+% Trial timeseries
+% trial_timeseries = cell(sum(Na),1);
+timeseries_aux = cellfun(@(c) reshape(transpose(c), numel(c), []), ...
+        chopped_timeseries, fnOpts{:});
+timeseries_aux = arrayfun(@(x) single(cat(1, timeseries_aux{x,:})), ...
+    1:N_signals, fnOpts{:});
+
+ts_ref = arrayfun(@(x) types.untyped.ObjectView("/stimulus/presentation/"+x), tsNames);
+for cfn = tsNames(:)'
+    ccfn = ismember(tsNames, cfn);
+    
+    if strcmpi(cfn,'laser')
+        ts_aux = types.core.OptogeneticSeries();
+        ts_aux.site = types.untyped.SoftLink("/general/optogenetics/"+cfn);
+%         ts_aux = types.core.OptogeneticSeries(...
+%             'data', timeseries_aux(:,ccfn), ...
+%             'data_unit', 'ADC_16bit', ...
+%             'description', cfn+" stimulation", ...
+%             'timestamps', timestamps, ...
+%             'timestamps_unit', 'seconds', ...
+%             'site', types.untyped.SoftLink("/general/optogenetics/"+cfn));
+    else
+        ts_aux = types.core.TimeSeries();
+    end
+    ts_aux.data = timeseries_aux{ccfn};
+    ts_aux.data_unit = 'ADC_16bits';
+    ts_aux.description = cfn+" stimulation";
+    ts_aux.timestamps = timestamps_all(:);
+    
+    nwbObj.stimulus_presentation.set(cfn, ts_aux);
+end
+
+trial_timeseries = arrayfun(@(x) {ts_ref(1) int64(x) int64(trial_samples); ...
+               ts_ref(2) int64(x) int64(trial_samples)}, ...
+               cat(1, trial_start_sample{:}), fnOpts{:});
+%% Trial epoch
 % Though TimeIntervals is a subclass of the DynamicTable type, we opt for
 % populating the Dynamic Table data by column instead of using `addRow`
 % here because of how the data is formatted. DynamicTable is flexible
 % enough to accomodate both styles of data conversion.
 trials_epoch = types.core.TimeIntervals(...
     'start_time', types.hdmf_common.VectorData('description', ...
-        'Start time of epoch, in seconds.'),...
+        'Start time of epoch, in seconds.', ...
+        'data', cat(1, trial_start_time_per_cond{:})),...
     'colnames', [data.trialTypeStr; data.trialPropertiesHash.keyNames .';...
-        {'start_time'; 'stop_time'; 'acquisition'; 'timeseries'}],...
+    {'start_time'; 'stop_time'; 'acquisition'; 'timeseries'}],...
     'description', 'trial data and properties', ...
     'id', types.hdmf_common.ElementIdentifiers(),...
     'timeseries', types.core.TimeSeriesReferenceVectorData('description', 'Index into timeseries Data'),...
@@ -115,7 +155,7 @@ trials_epoch.id.data = data.trialIds;
 for i=1:length(data.trialTypeStr)
     trials_epoch.vectordata.set(data.trialTypeStr{i}, ...
         types.hdmf_common.VectorData('data', data.trialTypeMat(i,:),...
-            'description', data.trialTypeStr{i}));
+        'description', data.trialTypeStr{i}));
 end
 
 for i=1:length(data.trialPropertiesHash.keyNames)
