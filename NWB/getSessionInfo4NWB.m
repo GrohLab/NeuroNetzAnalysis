d2de9370e16277e5e0e39391ce987912969c7a1c
Emilio Isaías-Camacho <isaias-camacho@physiologie.uni-heidelberg.de>
2023-03-07 11:22:36 +0100

    Initialised NWB object

--- NWB/getSessionInfo4NWB.m
+++ NWB/getSessionInfo4NWB.m
@@ -1,29 +1,63 @@
-function [nwbObj] = getSessionInfo4NWB(sessionPath)
-
+function [nwbObj] = getSessionInfo4NWB(sessionPath, varargin)
+%GETSESSIONINFO4NWB prepares a NwbFile object to store all information for
+%the PLOS Biology SC anatomy paper.
+%% Validate inputs
 p = inputParser;
 addRequired(p, 'sessionPath', @(x) exist(x, 'dir'))
-parse(p, sessionPath)
-sessionPath = p.Results.sessionPath;
+addParameter(p, 'Comment', "", @(x) isstring(x) | ischar(x))
 
+parse(p, sessionPath, varargin{:})
+sessionPath = p.Results.sessionPath;
+comment = p.Results.Comment;
+%% Auxiliary functions and variables
 pathHere = @(x) fullfile(sessionPath, x);
 look4this = @(x) dir(pathHere(x));
-
+%% Dig info out of the path
 pathParts = strsplit(sessionPath, filesep);
 batchFlag = contains(pathParts, "Batch");
 pathParts = pathParts(find(batchFlag):end);
-ephFlag = regexp(pathParts, "ephys\w*", "match");
+ephFlag = contains(pathParts, "ephys");
 
 rFiles = look4this("Recording*.bin");
-dt = arrayfun(@(x) getDates(x, "Recording",".bin"), rFiles); 
-dt = sort(dt); dt.Format = "default";
+if ~isempty(rFiles)
+    dt = arrayfun(@(x) getDates(x, "Recording",".bin"), rFiles);
+    dt = sort(dt); dt.Format = "default";
+end
+animal = "Mouse";
+if ephFlag(end) && exist('dt','var')
+    animal = regexp(pathParts, "[A-Z]+[0-9]+", "match");
+    animal = animal(~cellfun(@isempty, animal));
+    if ~isempty(animal)
+        if iscell(animal)
+            animal = string(animal{:});
+        end
+        animal = animal(1);
+    end
+    sessionDate = datetime(dt(1),"Format", "yyMMdd");
+else
+    sessionDate = regexp(pathParts, "[0-9]{6}","match");
+    sessionDate(cellfun(@isempty, sessionDate)) = [];
+    if iscell(sessionDate)
+        sessionDate = sessionDate{:};
+    end
+end
+identifier = animal + "_" + string(sessionDate);
+
+%% Create NWB object
 
 nwbObj = NwbFile();
 nwbObj.session_start_time = dt(1);
-nwbObj.identifier = '';
+nwbObj.identifier = identifier;
 nwbObj.general_lab = 'Prof. Alexander Groh';
+nwbObj.general_experiment_description
 nwbObj.general_institution = ['Medical Biophysics,'...
     ' Institute for Physiology and Pathophysiology, Heidelberg University'...
     ', Germany.'];
-nwbObj.session_description = sprintf("Awake head-fixed animal `%s` " + ...
-    "on the roller, session `%s`", animal, session);
+nwbObj.session_description = sprintf("Animal `%s`, session `%s`."+...
+    comment, animal, string(sessionDate));
+nwbObj.general_related_publications = ...
+    "Martín-Cortecero, J., Isaías-Camacho, E. U., Boztepe, B.," + ...
+    " Ziegler, K., Mease, R. A.,; Groh, A. (2023). " + ...
+    "Monosynaptic trans-collicular pathways for sensory-motor " + ...
+    "integration in the whisker system. DOI:10.1101/2022.08.30.505868";
 end
