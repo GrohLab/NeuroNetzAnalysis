a3ae88540529509ebef0ca9649fd255ed6eb4693
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2023-04-13 16:28:18 +0200

    Finished the movement probability measurement

--- computeMovementProbability.m
+++ computeMovementProbability.m
@@ -5,6 +5,8 @@
 fnOpts = {'UniformOutput', false};
 isWhiskOutlier = @(x, Qs, Qr) (Qs(1,:) - 1.5*Qr) > x(:)' | ...
     (Qs(2,:) + 1.5*Qr) < x(:)';
+sqzePge = @(mat, p) squeeze(mat(:,:,p));
+pge2cell = @(mat) arrayfun(@(pp) sqzePge(mat, pp), (1:size(mat,3))', fnOpts{:});
 [trigSubs, condIDs] = find(pairStim);
 condID = unique(condIDs); Nccond = numel(condID);
 
@@ -17,6 +19,9 @@ mvRng = cellfun(myRng, behStack);
 pk_loc = cellfun(@(bs, m) getWaveformCriticalPoints(bs, fr), behStack, fnOpts{:});
 pk_loc = cellfun(@(b) cellfun(@(t) t+bvWin(1), b, fnOpts{:}), pk_loc, fnOpts{:});
 
+pvpt = cellfun(@(bs, pl, m) arrayfun(@(tr) interp1(behTx, bs(:,tr), ...
+    pl{tr,1}, "cubic"), 1:size(pl,1), fnOpts{:}), behStack, pk_loc, fnOpts{:});
+
 right_side_flag = cellfun(@(pk) cellfun(@(tr) tr > 0, pk(:,1), ...
     fnOpts{:}), pk_loc, fnOpts{:});
 frstPk = cellfun(@(b) cellfun(@(t) t(find(t > 0.01, 1, "first")), b(:,1), ...
@@ -24,11 +29,8 @@ frstPk = cellfun(@(b) cellfun(@(t) t(find(t > 0.01, 1, "first")), b(:,1), ...
 frstPk_all = cellfun(@(b) cat(1, b{:}), frstPk, fnOpts{:});
 [~, trOrd] = cellfun(@(b) sort(b), frstPk_all, fnOpts{:});
 
-pvpt = cellfun(@(bs, pl, m) arrayfun(@(tr) interp1(behTx, bs(:,tr), ...
-    pl{tr,1}, "cubic"), trigSubs, fnOpts{:}), behStack, pk_loc, fnOpts{:});
-
-pdFlag = false(Ntr, Nccond, Nbs); pmFlag = pdFlag; mvFlag = pdFlag;
-pk_dist = cell(Ntr, Nccond, Nbs); rosFlag = pk_dist; froFlag = pdFlag;
+pdFlag = false(Ntr, Nccond, Nbs); pmFlag = pdFlag; froFlag = pdFlag;
+pk_dist = cell(Ntr, Nccond, Nbs); rosFlag = pk_dist; 
 rgFlag = true(size(froFlag));
 
 for cbs = 1:Nbs
@@ -64,10 +66,11 @@ for cbs = 1:Nbs
             end
         end
     end
-    mvFlag(:, cbs) = (pdFlag(:,cbs) | pmFlag(:,cbs) | froFlag(:,cbs)) & ...
-        rgFlag(:, cbs);
 end
-movStruct = struct('MovmentFlags', mvFlag, 'FirstMovement', frstPk_all, ...
-    'TrialOrder', trOrd, 'MedianFlag', pmFlag, 'DispersionFlag', pdFlag, ...
-    'PeakOutlierFlag', froFlag, 'RangeFlag', rgFlag);
+
+mvFlag = (pdFlag | pmFlag | froFlag) & rgFlag;
+movStruct = struct('MovmentFlags', pge2cell(mvFlag), ...
+    'FirstMovement', frstPk_all, 'TrialOrder', trOrd, ...
+    'MedianFlag', pge2cell(pmFlag), 'DispersionFlag', pge2cell(pdFlag), ...
+    'PeakOutlierFlag', pge2cell(froFlag), 'RangeFlag', pge2cell(rgFlag));
 end
