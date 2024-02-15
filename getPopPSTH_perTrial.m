084d7852a78701c56df6e392161461e740c8a50d
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2024-02-15 14:53:57 +0100

    Fixed an amateur bug

--- getPopPSTH_perTrial.m
+++ getPopPSTH_perTrial.m
@@ -1,4 +1,4 @@
-function psth_u = getPopPSTH_perTrial(rst, cf)
+function [psth_u, psth_tx, Na] = getPopPSTH_perTrial(rst, cf)
 psth_u = [];
 if ~checkRelSpkTmsStruct(rst)
     fprintf(1, 'No good relative spike time structure given!\n')
@@ -9,15 +9,15 @@ if ~checkConfigStruct(cf)
     return
 end
 
-binSize = configStructure.BinSize_s; vw = configStructure.Viewing_window_s;
+binSize = cf.BinSize_s; vw = cf.Viewing_window_s;
 fnOpts = {'UniformOutput', false};
 histOpts = {'BinWidth', binSize, 'BinLimits', vw};
 
-Ncl = size(relativeSpkTmsStruct(1).SpikeTimes, 1);
+Ncl = size(rst(1).SpikeTimes, 1);
 psth_u = arrayfun(@(c) arrayfun(@(tr) ...
-    histcounts([relativeSpkTmsStruct(c).SpikeTimes{:,tr}], ...
-    histOpts{:}), 1:size(relativeSpkTmsStruct(c).SpikeTimes, 2), ...
-    fnOpts{:}), 1:length(relativeSpkTmsStruct), fnOpts{:});
+    histcounts([rst(c).SpikeTimes{:,tr}], ...
+    histOpts{:}), 1:size(rst(c).SpikeTimes, 2), ...
+    fnOpts{:}), 1:length(rst), fnOpts{:});
 psth_u = cellfun(@(c) cat(1, c{:})/(Ncl*binSize), psth_u, fnOpts{:});
 
 [Na, Nt] = size(psth_u{1}); Nc = length(psth_u);
