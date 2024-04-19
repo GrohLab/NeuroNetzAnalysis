670c35c84bc0359550509cc38f9f307d60ce552d
Emilio Isaías-Camacho <isaias-camacho@physiologie.uni-heidelberg.de>
2024-04-19 09:55:41 +0200

    Vector support

--- evalgauss.m
+++ evalgauss.m
@@ -7,7 +7,7 @@ function prob = evalgauss(x,mu,sig)
 % \frac{1}{2\pi^{\frac{d}{2}}\sigma^\frac{1}{2}}
 % e^{-\frac{\left(x_i-\mu\right)'\left(x_i-\mu\right)}{2\sigma}}$
 %% Function
-auxc = 1/sqrt(2*pi*sig);
-auxe = ((x - mu).^2)/(2*sig);
-prob = auxc * exp(-auxe);
+auxc = 1./sqrt(2*pi*sig);
+auxe = ((x - mu).^2)./(2*sig);
+prob = auxc .* exp(-auxe);
 end
