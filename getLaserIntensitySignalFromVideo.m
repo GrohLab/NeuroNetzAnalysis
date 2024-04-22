3e1e63a67a14843cefae7a3452ed0a8ec584690c
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2024-04-22 13:54:11 +0200

    Preparations for running on the HPC

--- getLaserIntensitySignalFromVideo.m
+++ getLaserIntensitySignalFromVideo.m
@@ -4,12 +4,17 @@ Nf = video_obj.NumFrames;
 wd = video_obj.Width;
 ht = video_obj.Height;
 
-lsrInt = zeros( Nf, 1 , 'uint8' );
+lsrInt = zeros( Nf, 1, 'uint8' );
 mu_lcoords = round( mean( dlc_table.laser ) );
-
-mem = memory; maxMem = mem.MemAvailableAllArrays;
 frameByte = ht * wd * 3;
-bufferFrames = floor( (maxMem/frameByte) * 0.4 );
+
+try
+    mem = memory; maxMem = mem.MemAvailableAllArrays;
+    bufferFrames = floor( (maxMem/frameByte) * 0.4 );
+catch
+    % No memory module installed. Considering 32 GB of RAM memory
+    bufferFrames = floor( (32e9/frameByte) * 0.4 );
+end
 
 frames = zeros(ht, wd, 3, bufferFrames, 'uint8');
 
@@ -17,6 +22,8 @@ frCount = 0;
 while hasFrame( video_obj )
     if frCount + bufferFrames > Nf
         bufferFrames = Nf - frCount;
+    elseif frCount == Nf
+        bufferFrames = frCount;
     end
     fprintf(1, 'Reading %d/%d frames...\n', frCount + bufferFrames, Nf)
     frames(:,:,:,1:bufferFrames) = read( video_obj, frCount + [1, bufferFrames]);
