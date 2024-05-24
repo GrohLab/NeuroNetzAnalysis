9741ed05c1a3ac7083cbdef0203e69c9278af929
Emilio Isaías-Camacho <isaias-camacho@physiologie.uni-heidelberg.de>
2024-05-24 13:32:40 +0200

    Trial proportion + amplitude index

--- createBehaviourIndex.m
+++ createBehaviourIndex.m
@@ -5,6 +5,7 @@ function [pAreas, vertices, vFig] = createBehaviourIndex(behRes,varargin)
 %%
 p = inputParser;
 BSfields = {'ConditionName', 'Results'};
+fnOpts = {'UniformOutput', false};
 
 validateBS = @(x) isstruct(x) & all(isfield(x, BSfields));
 addRequired(p, 'behRes', validateBS)
@@ -16,55 +17,59 @@ behRes = p.Results.behRes;
 %%
 
 consCondNames = string({behRes.ConditionName});
+Nccond = numel( consCondNames ); clrMp = lines(Nccond);
 bsNames = {behRes(1).Results.BehSigName}; 
-[bAxis, vertices, Nccond, axTtlPos] = prepareRadialData(behRes);
-clrMp = lines(Nccond);
-
-[vFig, pd] = prepareRedialGraphics(bAxis, bsNames, axTtlPos);
-% plot data (pd)
-pAreas = pd(vertices, clrMp, consCondNames);
+mvPr = arrayfun(@(br) [br.Results.MovProbability], behRes, fnOpts{:});
+mvAm = arrayfun(@(br) [br.Results.AmplitudeIndex], behRes, fnOpts{:});
+mv = {mvPr, mvAm}; figNames = ["Trial proportion", "Amplitude index"];
+pAreas = zeros(Nccond, 2); vFig = gobjects(2, 1);
+for cmv = 1:numel(mv)
+    [bAxis, vertices] = prepareRadialData( behRes, mv{cmv} );
+    
+    [vFig(cmv), pd] = prepareRedialGraphics( bAxis, bsNames, figNames(cmv) );
+    % plot data (pd)
+    pAreas(:,cmv) = pd(vertices, clrMp, consCondNames);
+end
 
 end
 
-function [bAxis, vertices, Nccond, btmx] = prepareRadialData(behRes)
+function [bAxis, vertices, Nccond] = prepareRadialData(behRes, medi)
 fnOpts = {'UniformOutput', false};
 Nccond = numel(behRes);
-mvPr = arrayfun(@(br) [br.Results.MovProbability], behRes, fnOpts{:});
-mvPr = cat(1, mvPr{:});
+mvPr = cat(1, medi{:});
 Nb = size(mvPr, 2); radAxis = (0:Nb-1)*(2*pi/Nb); 
-bAxis = exp(1i*radAxis(:)); btmx = 1.15*max(mvPr,[],'all');
+bAxis = exp(1i*radAxis(:));
 vertices = arrayfun(@(c) mvPr(c,:).*transp(bAxis), 1:Nccond, fnOpts{:});
 vertices = cat(1, vertices{:});
 end
 
-function [vFig, fpd] = prepareRedialGraphics(bAxis, bsNames, btmx)
+function [vFig, fpd] = prepareRedialGraphics(bAxis, bsNames, figName)
 axOpts = {'Box', 'off', 'Color', 'none'};
 lgOpts = [axOpts(:)', {'Location'}, {'best'}];
-vFig = figure('Name', 'Behaviour dimensions', 'Color', 'w'); 
+txOpts = {'HorizontalAlignment','center', 'VerticalAlignment', 'middle', ...
+    'Rotation'};
+vFig = figure('Name', figName, 'Color', 'w'); 
 ax = axes('Parent', vFig, 'NextPlot', 'add', axOpts{:});
 plot([zeros(size(bAxis)), real(bAxis)]', ...
     [zeros(size(bAxis)), imag(bAxis)]', 'LineWidth', 0.3, ...
     'Color', 0.3*ones(1,3), 'LineStyle', ':'); 
-text(real(bAxis)*btmx*1.4, imag(bAxis)*btmx*1.4, string(bsNames), ...
-    'HorizontalAlignment','center')
-tcks = round(btmx,1)*(1:4)/4;
-arrayfun(@(r) text(real(bAxis(r))*tcks, imag(bAxis(r))*tcks,'|', ...
-    'Rotation', rad2deg(angle(bAxis(r))), ...
-    'HorizontalAlignment','center', ...
-    'VerticalAlignment','middle'), 1:size(bAxis,1)); 
-text(tcks, zeros(numel(bAxis),1), string(tcks), 'HorizontalAlignment','center', ...
-    'VerticalAlignment','top')
+arrayfun(@(c,t,a) text(real(c)*1.1, imag(c)*1.1, t, txOpts{:}, a ), ...
+    bAxis, string( bsNames(:) ), (180*angle( bAxis )/pi) - 90 );
+tcks = (1:4)/4;
+arrayfun(@(r) text(real(bAxis(r))*tcks, imag(bAxis(r))*tcks,sprintf("%c",743), ...
+    txOpts{:}, rad2deg(angle(bAxis(r)))), 1:size(bAxis,1));
+text(tcks, zeros(numel(tcks),1), string(tcks), txOpts{1:3}, 'top' )
 set(ax, axOpts{:})
 fpd = @plotData;
     function pAreas = plotData(vertices, clrMp, consCondNames)
         [Nccond, Nb] = size(vertices);
-        pObj = arrayfun(@(r) patch(real(vertices(r,:)), imag(vertices(r,:)), ones(1,Nb), ...
-            'facealpha', 0.1, 'EdgeColor', clrMp(r,:), 'FaceColor', clrMp(r,:), ...
-            'LineStyle', '-.'), 1:Nccond);
+        pObj = arrayfun(@(r) patch( real( vertices(r,:) ), ...
+            imag( vertices(r,:) ), ones( 1, Nb ), ...
+            'FaceAlpha', 0.1, 'EdgeColor', clrMp(r,:), ...
+            'FaceColor', clrMp(r,:), 'LineStyle', '-.'), 1:Nccond);
         pShape = arrayfun(@(p) polyshape(p.Vertices(:,1), p.Vertices(:,2)), pObj);
         pAreas = area(pShape); set(ax, axOpts{:}, 'Visible', 'off')
-        axis(ax, (2*btmx*double(btmx<0.5) + double(btmx>0.5))*[-1,1,-1,1], ...
-            'square'); 
+        axis(ax, [-1,1,-1,1], 'square'); 
         lgObj = legend(pObj, consCondNames + " " + string(round(pAreas,3))); 
         set(lgObj, lgOpts{:})
     end
