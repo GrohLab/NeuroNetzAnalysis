e30c7e6e8940517bac1f6e342a315c28489206dd
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2024-06-13 17:41:55 +0200

    Need to transform to a comb

--- iir50NotchFilter.m
+++ iir50NotchFilter.m
@@ -2,6 +2,7 @@ function [csig] = iir50NotchFilter(signal,sampling_frequency)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 % w0 = cut_frequency / (sampling_frequency/2)
+
 w0 = 100/sampling_frequency;
 % bw is the bandwidth of the notch using a 'Q' factor.
 bw = w0/35;
