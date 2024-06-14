dff29d7f70703a9dc99a3f79455378fa38ac04b5
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2024-06-14 15:06:03 +0200

    Comb filters addition

---
+++ iirCombFilter.m
@@ -0,0 +1,35 @@
+function [signal_out] = iirCombFilter( signal, sampling_frequency, varargin )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+p = inputParser;
+
+addRequired(p, 'signal', @(x) isvector( x ) && isnumeric( x ) );
+addRequired(p, 'sampling_frequency', @(x) isnumeric( x ) && x > 0 )
+addParameter(p, 'Q', 35, @(x) isnumeric( x ) && isscalar( x ) && x > 0 );
+addParameter(p, 'W0', 50, @(x) isnumeric( x ) && isscalar( x ) && ...
+    x > 0 && x < (sampling_frequency/2) );
+
+parse(p, signal, sampling_frequency, varargin{:} );
+
+signal = p.Results.signal;
+sampling_frequency = p.Results.sampling_frequency;
+Q = p.Results.Q;
+w0 = p.Results.W0;
+
+fnOpts = {'UniformOutput', false};
+
+N_notch = sampling_frequency / (2*w0);
+
+wo = (1:floor( N_notch) )*w0;
+[b, a] = arrayfun(@(w) iirnotch( w, (2*w)/Q ... Notch bandwidth using a 'Q' factor.
+    ), (2*wo)/sampling_frequency, fnOpts{:} );
+
+fprintf( 1, 'Filtering the signal for 50 Hz and its harmonics...' )
+signal_out = signal;
+for cfb = 1:numel(b)
+    signal_out = filtfilt( b{cfb}, a{cfb}, signal_out );
+end
+fprintf(' done!\n');
+
+end
