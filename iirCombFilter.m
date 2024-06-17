9d71d8c604ee7f8cc2a1e101d89bbdf9bf28bea5
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2024-06-17 10:19:17 +0200

    Added a verbose option

--- iirCombFilter.m
+++ iirCombFilter.m
@@ -9,6 +9,7 @@ addRequired(p, 'sampling_frequency', @(x) isnumeric( x ) && x > 0 )
 addParameter(p, 'Q', 35, @(x) isnumeric( x ) && isscalar( x ) && x > 0 );
 addParameter(p, 'W0', 50, @(x) isnumeric( x ) && isscalar( x ) && ...
     x > 0 && x < (sampling_frequency/2) );
+addParameter(p, 'verbose', true, @(x) islogical( x ) && isscalar( x ) );
 
 parse(p, signal, sampling_frequency, varargin{:} );
 
@@ -16,6 +17,7 @@ signal = p.Results.signal;
 sampling_frequency = p.Results.sampling_frequency;
 Q = p.Results.Q;
 w0 = p.Results.W0;
+verbose = p.Results.verbose;
 
 fnOpts = {'UniformOutput', false};
 
@@ -25,11 +27,17 @@ wo = (1:floor( N_notch) )*w0;
 [b, a] = arrayfun(@(w) iirnotch( w, (2*w)/Q ... Notch bandwidth using a 'Q' factor.
     ), (2*wo)/sampling_frequency, fnOpts{:} );
 
-fprintf( 1, 'Filtering the signal for 50 Hz and its harmonics...' )
+if verbose
+    fprintf( 1, 'Filtering the signal for 50 Hz and its harmonics...' )
+end
+
 signal_out = signal;
 for cfb = 1:numel(b)
     signal_out = filtfilt( b{cfb}, a{cfb}, signal_out );
 end
-fprintf(' done!\n');
+
+if verbose
+    fprintf(' done!\n');
+end
 
 end
