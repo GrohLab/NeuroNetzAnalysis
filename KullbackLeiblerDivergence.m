2721af4f2325dc9d655241135e5204c8511bf704
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2024-06-19 17:49:04 +0200

    Validation for infinite division

--- KullbackLeiblerDivergence.m
+++ KullbackLeiblerDivergence.m
@@ -1,16 +1,10 @@
 function kbd = KullbackLeiblerDivergence(P1,P2)
 % KULLBACKLEIBLERDIVERGENCE returns the similarity measure between two
 % probability distributions
-if abs(sum(P1) - 1.0) > 1e-5 || sum(~P1)
-    P1(P1 == 0) = range(P1)*1e-6;
-    P1 = P1/sum(P1);
-end
+P1 = checkDist( P1 );
 if length(P1) == length(P2)
     N = length(P1);
-    if abs(sum(P2) - 1) > 1e-3 || sum(~P2)
-        P2(P2 == 0) = range(P2)*1e-6;
-        P2 = P2/sum(P2);
-    end
+    P2 = checkDist( P2 );
 else
     kbd = NaN;
     fprintf('! The distributions have different resolutions (different length)!\n')
@@ -18,5 +12,17 @@ else
     return;
 end
 aux = rdivide(P1,P2);
-kbd = dot(P1,log2(aux))/log2(N);
+if any( isinf(aux) )
+    kbd = dot( P1, log2(P1) - log2(P2) ) / log2(N);
+else
+    kbd = dot(P1,log2(aux))/log2(N);
+end
+
+end
+
+function P = checkDist(P)
+if abs(sum(P) - 1.0) > 1e-5 || sum(~P)
+    P(P == 0) = range(P)*1e-6;
+    P = P/sum(P);
+end
 end
