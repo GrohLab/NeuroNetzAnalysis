8857a44f7fd626d0c5e35df7bdc568614558f8fe
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2024-09-04 15:51:21 +0200

    Improved performance of PSTH computation

--- getPSTH.m
+++ getPSTH.m
@@ -26,7 +26,7 @@ function [PSTH, trig, sweeps, timeAxis] =...
 %     timeaxis - is a 1xM array.
 % 
 %   Emilio Isaias-Camacho @ GrohLab 2019
-
+my_xor = @(x) xor( x(:,1), x(:,2) );
 if binSz > 1
     disp('Assuming bin size given in milliseconds and not in seconds.')
     binSz = binSz * 1e-3;
@@ -35,22 +35,28 @@ end
 % Computing the size of the PSTH stack
 [Ne, Nt, Na] = size(discreteStack);
 % Getting the raw PSTHs and kicking out the undesired trials
-auxCounts = sum(discreteStack(:,:,~kIdx),3);
-PSTH = zeros(Ne-1,ceil(diff(timeLapse)/binSz));
-% trig = zeros(1,ceil(Nt/binEls));
+%auxCounts = sum(discreteStack(:,:,~kIdx),3);
+Nb = ceil(diff(timeLapse)/binSz);
 sweeps = Na - sum(kIdx);
-trig = auxCounts(1,:)/sum(~kIdx);
-% Binned time axis
-timeAxis = (0:size(PSTH,2)-1)*binSz + timeLapse(1);
+trig = sum(discreteStack(1,:,~kIdx),3)/sum(~kIdx);
+
 txfs = (0:Nt-1)/fs + timeLapse(1);
-for ce = 2:Ne
-    tmVals = arrayfun(@(x,y) repmat(x,y,1), txfs, auxCounts(ce,:),...
-        'UniformOutput',0); tmVals = cat(1,tmVals{:});
-    h = histcounts(tmVals,'BinWidth',binSz,'BinLimits',timeLapse);
-    try
-        PSTH(ce-1,:) = h;
-    catch
-        PSTH(ce-1,1:numel(h)) = h;
-    end
+condStack = discreteStack(2:end,:,~kIdx);
+PSTH = zeros( Ne-1, Nb );
+bin_centres = (1:size(PSTH,2))'.^[1,0] * [1; -0.5] * binSz + timeLapse(1);
+bin_edges = [bin_centres - binSz/2, bin_centres + binSz/2];
+parfor cb = 1:Nb
+    bidx = my_xor( (txfs < bin_edges(cb,:)')' );
+    PSTH(:,cb) = sum( condStack( :, bidx, : ), [2,3] );
 end
+% for ce = 2:Ne
+%     tmVals = arrayfun(@(x,y) repmat(x,y,1), txfs, auxCounts(ce,:),...
+%         'UniformOutput',0); tmVals = cat(1,tmVals{:});
+%     h = histcounts(tmVals,'BinWidth',binSz,'BinLimits',timeLapse);
+%     try
+%         PSTH(ce-1,:) = h;
+%     catch
+%         PSTH(ce-1,1:numel(h)) = h;
+%     end
+% end
 end
