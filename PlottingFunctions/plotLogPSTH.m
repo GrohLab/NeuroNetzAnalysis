1a39da928ac8165dd35df5c4ec1b80d9426bfd49
Emilio Isaías-Camacho <isaias-camacho@physiologie.uni-heidelberg.de>
2024-09-05 11:02:29 +0200

    Full MImatrix

--- PlottingFunctions/plotLogPSTH.m
+++ PlottingFunctions/plotLogPSTH.m
@@ -159,6 +159,7 @@ if Ncond > 1
     lgnd = legend(prmAx(Nperm*2), [bP, bN]); set(lgnd, lgOpts{:})
     ylabel(prmAx(Nperm+1), 'Modulation Index')
     permFig.Visible = 'on';
+    ylim( prmAx(1:2:end), [0, Ncl] + 0.5 )
     figs(2) = permFig;
 end
 end
