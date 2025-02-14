224e6fd9ade49d7122160617702d1a5fd88d1dce
Emilio Isaías-Camacho <isaias-camacho@physiologie.uni-heidelberg.de>
2025-02-14 16:00:36 +0100

    Omit nans

--- goodnessFit2.m
+++ goodnessFit2.m
@@ -2,8 +2,9 @@ function [ rsq ] = goodnessFit2( pts, pts_hat, dim )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-SSEt = squeeze( sum( ( pts - pts_hat ).^2, dim ) );
-SSTt = squeeze( sum( ( pts - mean( pts, dim ) ).^2, dim ) );
+SSEt = squeeze( sum( ( pts - pts_hat ).^2, dim, "omitmissing" ) );
+SSTt = squeeze( sum( ( pts - mean( pts, dim, "omitmissing" ) ).^2, ...
+    dim, "omitmissing" ) );
 rsq = 1 - (SSEt./SSTt);
 
 end
