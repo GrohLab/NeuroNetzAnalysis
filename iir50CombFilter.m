5ac1adac2da12436a15cec3a46456bb78d62943a
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2026-03-27 12:57:31 +0100

    Limiting the filter bank to a 1/4 of Fq

--- iir50CombFilter.m
+++ iir50CombFilter.m
@@ -6,18 +6,19 @@ p = inputParser;
 
 addRequired(p, 'signal', @(x) isvector( x ) && isnumeric( x ) );
 addRequired(p, 'sampling_frequency', @(x) isnumeric( x ) && x > 0 )
-addParameter(p, 'Q', 35, @(x) isnumeric( x ) && isscalar( x ) && x > 0 );
+addParameter(p, 'bandwidth', 1, @(x) isnumeric( x ) && isscalar( x ) && ...
+    x > 0 && x <= 1);
 
 parse(p, signal, sampling_frequency, varargin{:} );
 
 signal = p.Results.signal;
 sampling_frequency = p.Results.sampling_frequency;
-Q = p.Results.Q;
+bw = p.Results.bandwidth;
 
 fnOpts = {'UniformOutput', false};
 
-wo = 50:50:(sampling_frequency/2); wo(wo == sampling_frequency/2) = [];
-[b, a] = arrayfun(@(w) iirnotch( w, (2*w)/Q ... Notch bandwidth using a 'Q' factor.
+wo = 50:50:(sampling_frequency/8); wo(wo == sampling_frequency/2) = [];
+[b, a] = arrayfun(@(w) iirnotch(w, bw/(sampling_frequency/2) ...
     ), (2*wo)/sampling_frequency, fnOpts{:} );
 
 fprintf( 1, 'Filtering the signal for 50 Hz and its harmonics...' )
